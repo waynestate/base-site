1f23e20aaba8baa480ad23813ed9efec0e151f18
Robert Vrabel <al2604@wayne.edu>
2017-10-24 15:13:12 -0400

    Fix if no profiles are found

--- resources/views/directory.blade.php
+++ resources/views/directory.blade.php
@@ -5,11 +5,11 @@
 
     {!! $page['content']['main'] !!}
 
-    @foreach($profiles as $key => $profiles)
+    @forelse($profiles as $key => $profiles)
         <h1>{{ $key }}</h1>
 
         <div class="row small-up-2 medium-up-3">
-            @forelse((array)$profiles as $profile)
+            @foreach((array)$profiles as $profile)
                 <div class="columns profile">
                     <a href="/{{ ($site['subsite-folder'] !== null) ? $site['subsite-folder'] : '' }}profile/{{ $profile['data']['AccessID'] }}" class="profile-img" style="background-image: url('{{ $profile['data']['Picture']['url'] or '/_resources/images/no-photo.svg' }}');" alt="{{ $profile['data']['First Name'] }} {{ $profile['data']['Last Name'] }}"></a>
                     <a href="/{{ ($site['subsite-folder'] !== null) ? $site['subsite-folder'] : '' }}profile/{{ $profile['data']['AccessID'] }}">{{ $profile['data']['First Name'] }} {{ $profile['data']['Last Name'] }}</a>
@@ -18,9 +18,9 @@
                         <span>{{ $profile['data']['Title'] }}</span>
                     @endif
                 </div>
-            @empty
-                <p>No profiles found.</p>
-            @endforelse
+            @endforeach
         </div>
-    @endforeach
+    @empty
+        <p>No profiles found.</p>
+    @endforelse
 @endsection
