ec28f6a32786254b83a0bc5f396df6555257d8b6
Chris Pelzer <chrispelzer@gmail.com>
2017-11-13 14:37:01 -0500

    add wsuheader 2.0

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,10 @@
+<div class="wsufooter">
+    <div class="wsuwrap">
+        <div class="privacy">
+            <p><a href="https://wayne.edu/policies/">Privacy and University Policies</a></p>
+        </div>
+        <div class="copyright">
+            <p><a href="https://wayne.edu/">Wayne State University</a> &copy; 2017</p>
+        </div>
+    </div>
+</div>
