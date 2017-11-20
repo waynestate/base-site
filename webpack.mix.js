7b2a0db7cb940bd0ddfc5f66503ca18f6d3bad50
Robert Vrabel <al2604@wayne.edu>
2017-11-20 14:21:46 -0500

    reloading with npm run watch-poll

--- webpack.mix.js
+++ webpack.mix.js
@@ -57,17 +57,20 @@ fs.symlink(
 
 // Compile JS and SCSS
 mix.js('resources/js/main.js', 'public/_resources/js')
-   .sass('resources/scss/main.scss', 'public/_resources/css');
-
-// We
-mix.options({
-    processCssUrls: false
-});
-
-// Auto refresh web browser when changes take place
-mix.browserSync({
-    proxy: 'https://' + package.name + '.wayne.local'
-});
+   .sass('resources/scss/main.scss', 'public/_resources/css')
+   .browserSync({
+        proxy: 'https://' + package.name + '.wayne.local',
+        open: false,
+        files: [
+            'app/**/*.php',
+            'resources/views/**/*.php',
+            'public/_resources/js/**/*.js',
+            'public/_resources/css/**/*.css'
+        ]
+    })
+    .options({
+        processCssUrls: false
+    });
 
 if (mix.inProduction()) {
     mix.version();
@@ -121,37 +124,3 @@ mix.webpackConfig({
         ]),
     ]
 });
-
-// Full API
-// mix.js(src, output);
-// mix.react(src, output); <-- Identical to mix.js(), but registers React Babel compilation.
-// mix.ts(src, output); <-- Requires tsconfig.json to exist in the same folder as webpack.mix.js
-// mix.extract(vendorLibs);
-// mix.sass(src, output);
-// mix.standaloneSass('src', output); <-- Faster, but isolated from Webpack.
-// mix.fastSass('src', output); <-- Alias for mix.standaloneSass().
-// mix.less(src, output);
-// mix.stylus(src, output);
-// mix.postCss(src, output, [require('postcss-some-plugin')()]);
-// mix.browserSync('my-site.dev');
-// mix.combine(files, destination);
-// mix.babel(files, destination); <-- Identical to mix.combine(), but also includes Babel compilation.
-// mix.copy(from, to);
-// mix.copyDirectory(fromDir, toDir);
-// mix.minify(file);
-// mix.sourceMaps(); // Enable sourcemaps
-// mix.version(); // Enable versioning.
-// mix.disableNotifications();
-// mix.setPublicPath('path/to/public');
-// mix.setResourceRoot('prefix/for/resource/locators');
-// mix.autoload({}); <-- Will be passed to Webpack's ProvidePlugin.
-// mix.webpackConfig({}); <-- Override webpack.config.js, without editing the file directly.
-// mix.then(function () {}) <-- Will be triggered each time Webpack finishes building.
-// mix.options({
-//   extractVueStyles: false, // Extract .vue component styling to file, rather than inline.
-//   globalVueStyles: file, // Variables file to be imported in every component.
-//   processCssUrls: true, // Process/optimize relative stylesheet url()'s. Set to false, if you don't want them touched.
-//   purifyCss: false, // Remove unused CSS selectors.
-//   uglify: {}, // Uglify-specific options. https://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
-//   postCss: [] // Post-CSS options: https://github.com/postcss/postcss/blob/master/docs/plugins.md
-// });
