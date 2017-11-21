847c6081e937b7062325869df023934823acc7d8
Robert Vrabel <al2604@wayne.edu>
2017-11-21 14:58:13 -0500

    Remove unnecessary dependencies Update the header Remove the bootstrap file to lessen the javascript file size

--- resources/js/main.js
+++ resources/js/main.js
@@ -1,12 +1,3 @@
-
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
-require('./bootstrap');
-
 // Import Modules
 import './modules/app';
 import './modules/table';
