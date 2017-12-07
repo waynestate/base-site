86dd2a4260ae8bc34038590c1d375d65b0389a74
Robert Vrabel <al2604@wayne.edu>
2017-12-07 14:54:36 -0500

    Add the newrelic middleware to run on every request

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends HttpKernel
      */
     protected $middleware = [
         \Illuminate\Foundation\Http\Middleware\CheckForMaintenanceMode::class,
+        \App\Http\Middleware\NewRelicMiddleware::class,
         \Illuminate\Foundation\Http\Middleware\ValidatePostSize::class,
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
