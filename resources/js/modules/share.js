90f10206fedb876615e31723a4d663014dabdf28
Robert Vrabel <al2604@wayne.edu>
2018-01-17 14:55:40 -0500

    Restructure the JS

--- resources/js/modules/share.js
+++ resources/js/modules/share.js
@@ -15,23 +15,28 @@ import jQuery from 'jquery';
          * Initialize
          */
         _init() {
-            if($('.addthis_sharing_toolbox').length > 0) {
-                if (!window.addthis) {
-                    // Asynchronous load of share icons
-                    var addthisScript = document.createElement('script');
-                    addthisScript.setAttribute('src', '//s7.addthis.com/js/300/addthis_widget.js#pubid=waynestate');
-                    addthisScript.setAttribute('async', 'async');
-                    document.body.appendChild(addthisScript);
-
-                    // Custom configuration for share icons
-                    var addthis_config = addthis_config || {};
-                    addthis_config.ui_508_compliant = true;
-                    addthis_config.ui_tabindex = 0;
-                }else{
-                    // Refreshes the share icons with the new URLs
-                    window.addthis.layers.refresh();
-                }
+            // Make sure addthis is desired on this page
+            if($('.addthis_sharing_toolbox').length == 0) {
+                return;
             }
+
+            if (!window.addthis) {
+                // Asynchronous load of share icons
+                var addthisScript = document.createElement('script');
+                addthisScript.setAttribute('src', '//s7.addthis.com/js/300/addthis_widget.js#pubid=waynestate');
+                addthisScript.setAttribute('async', 'async');
+                document.body.appendChild(addthisScript);
+
+                // Custom configuration for share icons
+                var addthis_config = addthis_config || {};
+                addthis_config.ui_508_compliant = true;
+                addthis_config.ui_tabindex = 0;
+
+                return;
+            }
+
+            // Refreshes the share icons with the new URLs
+            window.addthis.layers.refresh();
         }
     }
 
