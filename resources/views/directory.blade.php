a1d3c4c9450b98e58e36f01f49a3d29fa05b53fc
Nick DeNardis <nick.denardis@gmail.com>
2018-03-12 07:11:21 -0400

    :wheelchair: WCAG 2.0 AA updates

--- resources/views/directory.blade.php
+++ resources/views/directory.blade.php
@@ -11,7 +11,9 @@
         <div class="row small-up-2 medium-up-3">
             @foreach($profiles as $profile)
                 <div class="column profile">
-                    <a href="/{{ ($site['subsite-folder'] !== null) ? $site['subsite-folder'] : '' }}profile/{{ $profile['data']['AccessID'] }}" class="profile-img" style="background-image: url('{{ $profile['data']['Picture']['url'] or '/_resources/images/no-photo.svg' }}');" alt="{{ $profile['data']['First Name'] }} {{ $profile['data']['Last Name'] }}"></a>
+                    <a href="/{{ ($site['subsite-folder'] !== null) ? $site['subsite-folder'] : '' }}profile/{{ $profile['data']['AccessID'] }}" class="profile-img" style="background-image: url('{{ $profile['data']['Picture']['url'] or '/_resources/images/no-photo.svg' }}');">
+                        <span class="visuallyhidden">{{ $profile['data']['First Name'] }} {{ $profile['data']['Last Name'] }}</span>
+                    </a>
                     <a href="/{{ ($site['subsite-folder'] !== null) ? $site['subsite-folder'] : '' }}profile/{{ $profile['data']['AccessID'] }}">{{ $profile['data']['First Name'] }} {{ $profile['data']['Last Name'] }}</a>
 
                     @if(isset($profile['data']['Title']))
