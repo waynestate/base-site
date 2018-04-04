74e5a720864536c3ce9e2fb48c21c120affd36d5
Robert Vrabel <al2604@wayne.edu>
2018-04-04 09:31:54 -0400

    remove self closing tags

--- resources/views/partials/meta.blade.php
+++ resources/views/partials/meta.blade.php
@@ -1,22 +1,22 @@
-<meta name="Keywords" content="{{ isset($page['keywords']) ? $page['keywords'] : '' }}" />
-<meta name="Description" content="{{ isset($page['description']) ? $page['description'] : '' }}" />
-<meta http-equiv="last-modified" content="{{ isset($page['updated-at']) ? $page['updated-at'] : '' }}" />
+<meta name="Keywords" content="{{ isset($page['keywords']) ? $page['keywords'] : '' }}">
+<meta name="Description" content="{{ isset($page['description']) ? $page['description'] : '' }}">
+<meta http-equiv="last-modified" content="{{ isset($page['updated-at']) ? $page['updated-at'] : '' }}">
 
-<meta property="og:title" content="{{ isset($page['title']) ? $page['title'] : '' }}" />
-<meta property="og:image" content="{{ isset($meta['image']) ? $meta['image'] : '' }}" />
-<meta property="og:image:alt" content="{{ isset($meta['image_alt']) ? $meta['image_alt'] : '' }}" />
-<meta property="og:description" content="{{ isset($page['description']) ? $page['description'] : '' }}" />
-<meta property="og:url" content="{{ isset($server['url']) ? $server['url'] : '' }}" />
+<meta property="og:title" content="{{ isset($page['title']) ? $page['title'] : '' }}">
+<meta property="og:image" content="{{ isset($meta['image']) ? $meta['image'] : '' }}">
+<meta property="og:image:alt" content="{{ isset($meta['image_alt']) ? $meta['image_alt'] : '' }}">
+<meta property="og:description" content="{{ isset($page['description']) ? $page['description'] : '' }}">
+<meta property="og:url" content="{{ isset($server['url']) ? $server['url'] : '' }}">
 <meta property="og:type" content="article" />
-<meta property="og:updated_time" content="{{ isset($page['updated-at']) ? $page['updated-at'] : '' }}" />
-<meta property="og:site_name" content="{{ isset($site['title']) ? $site['title'] : '' }}" />
+<meta property="og:updated_time" content="{{ isset($page['updated-at']) ? $page['updated-at'] : '' }}">
+<meta property="og:site_name" content="{{ isset($site['title']) ? $site['title'] : '' }}">
 
 @if(!empty(config('app.facebook_profile_id')))
-<meta property="fb:profile_id" content="{{ config('app.facebook_profile_id') }}" />
+<meta property="fb:profile_id" content="{{ config('app.facebook_profile_id') }}">
 @endif
 
 @if(!empty(config('app.facebook_app_id')))
-<meta property="fb:app_id" content="{{ config('app.facebook_app_id') }}" />
+<meta property="fb:app_id" content="{{ config('app.facebook_app_id') }}">
 @endif
 
 <meta name="twitter:card" content="summary_large_image">
@@ -25,4 +25,4 @@
 <meta name="twitter:url" content="{{ isset($server['url']) ? $server['url'] : '' }}">
 <meta name="twitter:site" content="@waynestate">
 <meta name="twitter:image" content="{{ isset($meta['image']) ? $meta['image'] : '' }}">
-<meta property="article:published_time" content="{{ isset($page['updated-at']) ? $page['updated-at'] : '' }}" />
+<meta property="article:published_time" content="{{ isset($page['updated-at']) ? $page['updated-at'] : '' }}">
