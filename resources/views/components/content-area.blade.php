48138b07abc12df7404670f49b44af3451d53440
Robert Vrabel <al2604@wayne.edu>
2018-05-01 12:03:05 -0400

    Fix up the gutters to make them more accurate to what we currently have

--- resources/views/components/content-area.blade.php
+++ resources/views/components/content-area.blade.php
@@ -8,7 +8,7 @@
     @endif
 
     <div class="row flex">
-        <div class="md:w-1/4 px-4 main-menu @if($site_menu['meta']['has_selected'] == false && ((isset($show_site_menu) && $show_site_menu != true) || !isset($show_site_menu))) mt:hidden @endif" data-off-canvas id="page-menu" role="navigation"  aria-label="Page menu" tabindex="-1">
+        <div class="md:w-1/4 pl-4 main-menu @if($site_menu['meta']['has_selected'] == false && ((isset($show_site_menu) && $show_site_menu != true) || !isset($show_site_menu))) mt:hidden @endif" data-off-canvas id="page-menu" role="navigation"  aria-label="Page menu" tabindex="-1">
             @if(!empty($top_menu_output) && $site_menu !== $top_menu)
                 <div class="offcanvas-main-menu">
                     <ul>
@@ -32,7 +32,7 @@
             @endif
         </div>
 
-        <div class="w-full @if($site_menu['meta']['has_selected'] == false && ((isset($show_site_menu) && $show_site_menu != true) || !isset($show_site_menu)))md:w-full @else md:w-3/4 @endif content-area" data-off-canvas-content>
+        <div class="w-full px-4 @if($site_menu['meta']['has_selected'] == false && ((isset($show_site_menu) && $show_site_menu != true) || !isset($show_site_menu)))md:w-full @else md:w-3/4 @endif content-area" data-off-canvas-content>
             @if(!empty($hero) && ($site_menu['meta']['has_selected'] == true || config('app.hero_contained') === true))
                 @include('components.hero', ['images' => $hero, 'class' => 'hero--childpage'])
             @endif
