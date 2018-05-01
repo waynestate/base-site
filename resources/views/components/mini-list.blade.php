9d3b9a6e2be2a79001259cd5d96f48131a852a0d
Robert Vrabel <al2604@wayne.edu>
2018-05-01 09:57:54 -0400

    Convert more link

--- resources/views/components/mini-list.blade.php
+++ resources/views/components/mini-list.blade.php
@@ -4,7 +4,7 @@
     $url => string // '/listing.php'
     $link_text => string // 'More items'
 --}}
-<h2>{{ $heading ?? 'Resources' }}</h2>
+<h2 class="mb-4">{{ $heading ?? 'Resources' }}</h2>
 
 <ul class="listing">
     @foreach($items as $item)
@@ -14,4 +14,4 @@
     @endforeach
 </ul>
 
-@if(isset($url))<a href="{{ $url }}" class="more-link">{{ $link_text ?? 'View more' }}</a>@endif
+@if(isset($url))<a href="{{ $url }}" class="block my-4 underline hover:no-underline">{{ $link_text ?? 'View more' }}</a>@endif
