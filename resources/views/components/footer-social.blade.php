43ca8bff5604bc89d75ad0c68643b0251ca4cec5
Robert Vrabel <al2604@wayne.edu>
2018-05-01 15:18:24 -0400

    move social footer to classes

--- resources/views/components/footer-social.blade.php
+++ resources/views/components/footer-social.blade.php
@@ -1,12 +1,12 @@
 {{--
     $social => array // [['title','link']]
 --}}
-<div class="footer-social">
-    <ul class="row text-center">
+<div class="bg-blue-darkest pt-6 pb-4 mt-8">
+    <ul class="row list-reset text-center">
         @foreach($social as $item)
-            <li>
-                <a href="{{ $item['link'] }}" target="_blank" rel="noopener">
-                    <span class="icon-{{ strtolower($item['title']) }}"></span><p class="visually-hidden">{{ $item['title'] }}</p>
+            <li class="inline{{ $loop->last !== true ? ' mr-8' : '' }}">
+                <a href="{{ $item['link'] }}" target="_blank" rel="noopener" class="inline-block text-grey hover:text-white">
+                    <span class="icon-{{ strtolower($item['title']) }} table-cell align-middle text-3xl h-14 w-14 bg-grey-darkest rounded-full transition transition-property-bg transition-delay-none hover:bg-green-lightest hover:text-white"></span><p class="visually-hidden">{{ $item['title'] }}</p>
                 </a>
             </li>
         @endforeach
