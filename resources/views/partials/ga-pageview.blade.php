001e97755ed768582ee41dad1b56e3f28d3869be
Robert Vrabel <al2604@wayne.edu>
2018-05-01 10:43:11 -0400

    combine GA code

--- resources/views/partials/ga-pageview.blade.php
+++
@@ -1,8 +0,0 @@
-@if(config('app.env') != 'local')
-    <script>
-        @if(config('app.ga_code') != 'UA-' && config('app.ga_name') != null)
-        ga('{{ config('app.ga_name') }}.send', 'pageview', '{{ $server['path_with_query'] }}');
-        @endif
-        ga('allWayneState.send', 'pageview', '{{ $server['path_with_query'] }}');
-    </script>
-@endif
--- resources/views/partials/ga.blade.php
+++ resources/views/partials/ga.blade.php
@@ -10,4 +10,12 @@
 </script>
 @endif
 
-@include('partials.ga-pageview')
+@if(config('app.env') != 'local')
+    <script>
+        @if(config('app.ga_code') != 'UA-' && config('app.ga_name') != null)
+        ga('{{ config('app.ga_name') }}.send', 'pageview', '{{ $server['path_with_query'] }}');
+        @endif
+        ga('allWayneState.send', 'pageview', '{{ $server['path_with_query'] }}');
+    </script>
+@endif
+
