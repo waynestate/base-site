86c03059acf995245a21630d2fa8baead1ab595c
Robert Vrabel <al2604@wayne.edu>
2018-05-15 09:20:30 -0400

    Remove the other examples now that we've got the height correct

--- resources/views/components/image-button-list.blade.php
+++ resources/views/components/image-button-list.blade.php
@@ -8,15 +8,10 @@
     <div class="my-4 min-w-full">
         @if(!empty($image['link']))<a href="{{ $image['link'] }}" @if(empty($image['relative_url']))class="button expanded"@endif>@endif
             @if(!empty($image['relative_url']))
-                {{-- <img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-flickity-lazyload="{{ $image['relative_url'] }}" alt="{{ $image['title'] }}" /> --}}
-
-                {{-- @image($image['relative_url'], $image['title']) --}}
-
                 @if($loop->first == true)
                     <img src="{{ $image['relative_url'] }}" alt="{{ $image['title'] }}" />
                 @else
                     @image($image['relative_url'], $image['title'])
-                    {{-- <img class="min-w-full" src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==" data-flickity-lazyload="{{ $image['relative_url'] }}" alt="{{ $image['title'] }}" /> --}}
                 @endif
             @else
                 {{ $image['title'] }}
