a54f851af9a94f957fdcef74fec55f7ee8892829
Robert Vrabel <al2604@wayne.edu>
2018-05-17 09:35:07 -0400

    Add hero text

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -1,40 +1,20 @@
 {{--
     $images => array // ['relative_url']
-    $class => string // 'hero--childpage' if used on a childpage with left menu, and any additional classes
 --}}
-
-{{-- <div class="{{ (count($images) > 1)? 'rotate ' : '' }} hero{{ isset($class) ? ' ' . $class : '' }}">
-    @foreach($images as $image)
-        <div class="hero__slide">
-            @if($image == reset($images))<div class="hero__image" style="background-image: url('{{ $image['relative_url'] }}')">@else <div class="hero__image lazy" data-src="{{ $image['relative_url'] }}">@endif
-                @if(!empty($image['excerpt']) && config('app.hero_text_enabled') && in_array($page['controller'], config('app.hero_text_controllers')))
-                    <div class="hero__excerpt">
-                        <div class="row px-4">
-                            {{ $image['excerpt'] }} @if(!empty($image['link'])) <a href="{{ $image['link'] }}" class="hero__view-more">{{ config('app.hero_text_more') }}</a>@endif
-                        </div>
-                    </div>
-                @endif
-            </div>
-        </div>
-    @endforeach
-</div> --}}
-
-{{-- <div class="{{ (count($images) > 1)? 'ro1tate ' : '' }}">
-    @foreach($images as $image)
-        @if($loop->first === true)
-            <div style="background-image: url('{{ $image['relative_url'] }}')"></div>
-        @else
-            <div class="lazy" data-src="{{ $image['relative_url'] }}"></div>
-        @endif
-    @endforeach
-</div> --}}
 
 <div class="hero{{ !empty($images) && count($images) > 1 ? ' rotate' : '' }}{{ !empty($show_site_menu) && $show_site_menu === true ? ' contained' : ' full' }}">
     @foreach($images as $image)
         @if($loop->first === true)
-            <div class="carousel-cell" style="background-image: url('{{ $image['relative_url'] }}')"></div>
+            <div class="carousel-cell" style="background-image: url('{{ $image['relative_url'] }}')">
         @else
-            <div class="carousel-cell lazy" data-src="{{ $image['relative_url'] }}"></div>
+            <div class="carousel-cell lazy" data-src="{{ $image['relative_url'] }}">
         @endif
+
+            @if(!empty($image['excerpt']) && config('app.hero_text_enabled') && in_array($page['controller'], config('app.hero_text_controllers')))
+                <div class="bg-green-darker text-white p-4 absolute pin-b pin-l pin-r">
+                    {{ $image['excerpt'] }} @if(!empty($image['link'])) <a href="{{ $image['link'] }}" class="text-white underline font-bold pl-2 hover:no-underline">{{ config('app.hero_text_more') }}</a>@endif
+                </div>
+            @endif
+        </div>
     @endforeach
 </div>
