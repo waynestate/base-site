f0ba0a6db854213947c6e4ae858eed6886814b4f
Robert Vrabel <al2604@wayne.edu>
2018-05-18 15:40:29 -0400

    update colors

--- resources/views/components/footer-social.blade.php
+++ resources/views/components/footer-social.blade.php
@@ -5,7 +5,7 @@
     <ul class="row list-reset text-center">
         @foreach($social as $item)
             <li class="inline{{ $loop->last !== true ? ' mr-8' : '' }}">
-                <a href="{{ $item['link'] }}" target="_blank" rel="noopener" class="inline-block pt-1 text-white fill-current table-cell align-middle h-14 w-14 bg-green-light rounded-full transition transition-property-bg transition-delay-none hover:bg-green-lighter hover:text-white">
+                <a href="{{ $item['link'] }}" target="_blank" rel="noopener" class="inline-block pt-1 text-grey fill-current table-cell align-middle h-14 w-14 bg-green rounded-full transition transition-property-bg transition-delay-none hover:bg-green-lighter hover:text-white">
                     @svg($item['title'])
                 </a>
             </li>
