1954ade7908fd0501dfbcb0f700d0c5ea0e8a87a
Jenny Porada <jennyingles@wayne.edu>
2018-05-18 17:23:25 -0400

    Changing the social footer to flexbox

--- resources/views/components/footer-social.blade.php
+++ resources/views/components/footer-social.blade.php
@@ -1,14 +1,13 @@
 {{--
     $social => array // [['title','link']]
+    {{ $loop->last !== true ? ' mr-8' : '' }}
 --}}
-<div class="bg-green-darker pt-6 pb-4">
-    <ul class="row list-reset text-center">
-        @foreach($social as $item)
-            <li class="inline{{ $loop->last !== true ? ' mr-8' : '' }}">
-                <a href="{{ $item['link'] }}" target="_blank" rel="noopener" class="inline-block pt-1 text-green-lightest fill-current table-cell align-middle h-14 w-14 bg-green rounded-full transition transition-property-bg transition-delay-none hover:bg-green-lighter hover:text-white">
+<div class="bg-green-darker py-6">
+    <div class="row flex justify-center items-center">
+            @foreach($social as $item)
+                <a href="{{ $item['link'] }}" target="_blank" rel="noopener" class="inline-block h-10 w-10 md:h-14 md:w-14 mx-1 md:mx-6 text-green-lightest fill-current bg-green rounded-full transition transition-property-bg transition-delay-none hover:bg-green-lighter hover:text-white flex justify-center items-center">
                     @svg($item['title'])
                 </a>
-            </li>
-        @endforeach
-    </ul>
+            @endforeach
+        </div>
 </div>
