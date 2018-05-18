3f39f4ba94dbee0abb3fea428d21a6cd3f9a55df
Robert Vrabel <al2604@wayne.edu>
2018-05-18 15:36:32 -0400

    remove changed/updated and update footer social colors

--- resources/views/components/footer-social.blade.php
+++ resources/views/components/footer-social.blade.php
@@ -1,11 +1,11 @@
 {{--
     $social => array // [['title','link']]
 --}}
-<div class="bg-teal-darkest pt-6 pb-4">
+<div class="bg-green-darker pt-6 pb-4">
     <ul class="row list-reset text-center">
         @foreach($social as $item)
             <li class="inline{{ $loop->last !== true ? ' mr-8' : '' }}">
-                <a href="{{ $item['link'] }}" target="_blank" rel="noopener" class="inline-block pt-1 text-grey fill-current table-cell align-middle h-14 w-14 bg-grey-darkest rounded-full transition transition-property-bg transition-delay-none hover:bg-green-lightest hover:text-white">
+                <a href="{{ $item['link'] }}" target="_blank" rel="noopener" class="inline-block pt-1 text-white fill-current table-cell align-middle h-14 w-14 bg-green-light rounded-full transition transition-property-bg transition-delay-none hover:bg-green-lighter hover:text-white">
                     @svg($item['title'])
                 </a>
             </li>
