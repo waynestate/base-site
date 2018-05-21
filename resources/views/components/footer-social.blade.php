4276423ba2a720ebf8dc3ed4943686c7e89d4109
Robert Vrabel <al2604@wayne.edu>
2018-05-21 09:14:31 -0400

    slightly more space for small

--- resources/views/components/footer-social.blade.php
+++ resources/views/components/footer-social.blade.php
@@ -4,7 +4,7 @@
 <div class="bg-green-darker py-6">
     <div class="row flex justify-center items-center">
             @foreach($social as $item)
-                <a href="{{ $item['link'] }}" target="_blank" rel="noopener" class="flex justify-center items-center h-10 w-10 md:h-14 md:w-14 mx-1 md:mx-6 text-green-lightest fill-current bg-green rounded-full transition transition-property-bg transition-delay-none hover:bg-green-lighter hover:text-white">
+                <a href="{{ $item['link'] }}" target="_blank" rel="noopener" class="flex justify-center items-center h-10 w-10 md:h-14 md:w-14 mx-1 sm:mx-3 md:mx-6 text-green-lightest fill-current bg-green rounded-full transition transition-property-bg transition-delay-none hover:bg-green-lighter hover:text-white">
                     @svg($item['title'])
                 </a>
             @endforeach
