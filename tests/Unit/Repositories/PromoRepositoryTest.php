28f06e0ecf1b5d6e2bc5a544317874ac18b644d6
Robert Vrabel <al2604@wayne.edu>
2018-05-23 09:24:46 -0400

    Add test to cover using the main config for subsites

--- tests/Unit/Repositories/PromoRepositoryTest.php
+++ tests/Unit/Repositories/PromoRepositoryTest.php
@@ -43,6 +43,74 @@ class PromoRepositoryTest extends TestCase
     }
 
     /**
+     * @covers App\Repositories\PromoRepository::__construct
+     * @covers App\Repositories\PromoRepository::getRequestData
+     * @test
+     */
+    public function subsite_using_main_config()
+    {
+        // Fake return
+        $return = [
+            'promotions' => [
+                [
+                    'promo_item_id' => 1,
+                    'promo_group_id' => 3,
+                ],
+                [
+                    'promo_item_id' => 2,
+                    'promo_group_id' => 3,
+                ],
+                [
+                    'promo_item_id' => 3,
+                    'promo_group_id' => 2,
+                ],
+            ],
+        ];
+
+        // Create a fake data request
+        $data = app('Factories\Page')->create(1, [
+            'site' => [
+                'id' => 2,
+                'parent' => [
+                    'id' => 1,
+                ],
+            ],
+        ]);
+
+        // Build the config
+        config(['base.global_promos' => [
+            'main' => [
+                'contact' => [
+                    'id' => 1,
+                    'config' => 'limit:1',
+                ],
+                'social' => [
+                    'id' => 2,
+                    'config' => 'limit:1',
+                ],
+            ],
+            'subsites' => [
+                $data['site']['id'] => [
+                    'contact' => [
+                        'id' => 3,
+                    ],
+                ],
+            ],
+        ]]);
+
+        // Mock the connector and set the return
+        $wsuApi = Mockery::mock('Waynestate\Api\Connector');
+        $wsuApi->shouldReceive('sendRequest')->with('cms.promotions.listing', Mockery::type('array'))->once()->andReturn($return);
+
+        // Get the promos
+        $promos = app('App\Repositories\PromoRepository', ['wsuApi' => $wsuApi])->getRequestData($data);
+
+        // Make sure that the count is equal to the main config limit
+        $this->assertCount(1, $promos['contact']);
+        $this->assertCount(1, $promos['social']);
+    }
+
+    /**
      * @covers App\Repositories\PromoRepository::getHomepagePromos
      * @test
      */
