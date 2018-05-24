de477f92575a75032e8c97082a641fb4162f453e
Robert Vrabel <al2604@wayne.edu>
2018-05-24 11:16:29 -0400

    Move headings to global file Add margin bottom to hero so the heading is positioned properly Remove margin bottom from the headings so we rely on the global style

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -2,7 +2,7 @@
     $images => array // ['relative_url']
 --}}
 
-<div class="mt:mx-0{{ !empty($images) && count($images) > 1 ? ' rotate' : '' }}{{ !empty($show_site_menu) && $show_site_menu === true ? ' contained -mx-4' : ' full' }}">
+<div class="mb-4 mt:mx-0{{ !empty($images) && count($images) > 1 ? ' rotate' : '' }}{{ !empty($show_site_menu) && $show_site_menu === true ? ' contained -mx-4' : ' full' }}">
     @foreach($images as $image)
         <div class="pt-partial w-full bg-cover bg-top relative{{ $loop->first !== true ? ' lazy' : '' }}" @if($loop->first === true) style="background-image: url('{{ $image['relative_url'] }}')" @else data-src="{{ $image['relative_url'] }}"@endif>
             @if(!empty($image['excerpt']) && config('base.hero_text_enabled') && in_array($page['controller'], config('base.hero_text_controllers')))
