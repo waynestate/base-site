33d148e5f773db92a12870fe85e068ef836bc48b
Robert Vrabel <al2604@wayne.edu>
2018-05-24 09:26:51 -0400

    Add content class to the content coming from promotions and add a component class so we can target the links and make them white

--- resources/views/components/footer-contact.blade.php
+++ resources/views/components/footer-contact.blade.php
@@ -12,7 +12,7 @@
                         @if($info['link'] != '')</a>@endif
                     </h2>
 
-                    <div class="p-6 text-white">
+                    <div class="p-6 text-white content footer-contact">
                         {!! $info['description'] !!}
                     </div>
                 @endforeach
@@ -20,7 +20,7 @@
         @else
             <div class="row flex flex-wrap py-8">
                 @foreach($contact as $info)
-                <div class="w-full sm:w-1/3 px-4 py-4 text-white {{ $loop->last !== true ? 'border-b sm:border-r sm:border-0 border-grey' : '' }}">
+                    <div class="w-full sm:w-1/3 px-4 py-4 text-white content footer-contact {{ $loop->last !== true ? 'border-b sm:border-r sm:border-0 border-grey' : '' }}">
                         {!! $info['description'] !!}
                     </div>
                 @endforeach
