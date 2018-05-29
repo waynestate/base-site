6d671155602535c63a7088e3ff6f01204bc1a804
Robert Vrabel <al2604@wayne.edu>
2018-05-29 12:54:15 -0400

    Remove the menu-top class and references to it since its not needed. Removed the placeholder for the sticky menu since we aren't using it.

--- resources/js/modules/sticky.js
+++ resources/js/modules/sticky.js
@@ -3,10 +3,8 @@
 
     window.addEventListener('scroll', function() {
         if (window.pageYOffset >= document.querySelector('.wsuheader').offsetHeight) {
-            document.querySelector('.menu-top-placeholder').style.height = document.querySelector('div.menu-top').offsetHeight;
             document.querySelector('.menu-top-container').classList.add('w-full', 'fixed', 'pin-t', 'z-10');
         } else if(window.pageYOffset < document.querySelector('.wsuheader').offsetHeight) {
-            document.querySelector('.menu-top-placeholder').style.height = '0px';
             document.querySelector('.menu-top-container').classList.remove('w-full', 'fixed', 'pin-t', 'z-10');
         }
     });
