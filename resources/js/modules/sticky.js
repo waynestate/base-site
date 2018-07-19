79430c38043a63b2a62c1915b8bf9dd1093ed73a
Robert Vrabel <al2604@wayne.edu>
2018-07-19 14:18:56 -0400

    Start sticky header at 0 but if the WSU header exists then use that height as the offset.

--- resources/js/modules/sticky.js
+++ resources/js/modules/sticky.js
@@ -2,13 +2,19 @@
     "use strict";
 
     window.addEventListener('scroll', function() {
-        if (window.pageYOffset >= document.querySelector('.wsuheader').offsetHeight) {
+        var offsetHeight = 0;
+
+        if(document.querySelector('.wsuheader')) {
+            offsetHeight = document.querySelector('.wsuheader').offsetHeight;
+        }
+
+        if (window.pageYOffset >= offsetHeight) {
             document.getElementById('panel').style.marginTop = document.querySelector('.menu-top-container .row.flex').offsetHeight + 'px';
             document.querySelector('.menu-top-container').classList.add('w-full');
             document.querySelector('.menu-top-container').classList.add('fixed');
             document.querySelector('.menu-top-container').classList.add('pin-t');
             document.querySelector('.menu-top-container').classList.add('z-10');
-        } else if(window.pageYOffset < document.querySelector('.wsuheader').offsetHeight) {
+        } else if(window.pageYOffset < offsetHeight) {
             document.getElementById('panel').style.marginTop = '0px';
             document.querySelector('.menu-top-container').classList.remove('w-full');
             document.querySelector('.menu-top-container').classList.remove('fixed');
