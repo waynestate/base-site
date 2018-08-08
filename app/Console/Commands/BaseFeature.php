268e9a4544ac87560631697bfbdd16ec8e5a9c8a
Robert Vrabel <al2604@wayne.edu>
2018-08-08 08:19:47 -0400

    Refactor methods since they aren't going to be reused at all

--- app/Console/Commands/BaseFeature.php
+++ app/Console/Commands/BaseFeature.php
@@ -32,11 +32,15 @@ class BaseFeature extends Command
     public function controller()
     {
         $this->initializeStub('controller');
-        $this->replaceDescription();
         $this->replaceContract();
-        $this->replaceControllerName();
-        $this->replaceVariables();
-        $this->replaceView();
+        $this->stub = str_replace('Dummy Template', $this->feature.' Template', $this->stub);
+        $this->stub = str_replace('DummyController', $this->feature.'Controller', $this->stub);
+        $this->stub = str_replace(
+            ['$dummy', '$this->dummy', '$dummys', '$this->getDummys'],
+            ['$'.strtolower($this->feature), '$this->'.strtolower($this->feature), '$'.strtolower($this->feature).'s', '$this->get'.ucfirst(strtolower($this->feature)).'s'],
+            $this->stub
+        );
+        $this->stub = str_replace('DummyView', strtolower($this->feature), $this->stub);
 
         Storage::disk('base')->put('App\Http\Controllers\/'.$this->feature.'Controller.php', $this->stub);
     }
@@ -44,7 +48,7 @@ class BaseFeature extends Command
     public function contract()
     {
         $this->initializeStub('contract');
-        $this->replaceContractName();
+        $this->replaceContract();
         $this->stub = str_replace('getDummys', 'get'.ucfirst(strtolower($this->feature)).'s', $this->stub);
         $this->stub = str_replace('dummys', strtolower($this->feature).'s', $this->stub);
 
@@ -72,36 +76,7 @@ class BaseFeature extends Command
         $this->stub = Storage::disk('base')->get('stubs/'.$type.'.stub');
     }
 
-    public function replaceDescription()
-    {
-        $this->stub = str_replace('Dummy Template', $this->feature.' Template', $this->stub);
-    }
-
-    public function replaceControllerName()
-    {
-        $this->stub = str_replace('DummyController', $this->feature.'Controller', $this->stub);
-    }
-
     public function replaceContract()
-    {
-        $this->stub = str_replace('DummyRepositoryContract', $this->feature.'RepositoryContract', $this->stub);
-    }
-
-    public function replaceVariables()
-    {
-        $this->stub = str_replace(
-            ['$dummy', '$this->dummy', '$dummys', '$this->getDummys'],
-            ['$'.strtolower($this->feature), '$this->'.strtolower($this->feature), '$'.strtolower($this->feature).'s', '$this->get'.ucfirst(strtolower($this->feature)).'s'],
-            $this->stub
-        );
-    }
-
-    public function replaceView()
-    {
-        $this->stub = str_replace('DummyView', strtolower($this->feature), $this->stub);
-    }
-
-    public function replaceContractName()
     {
         $this->stub = str_replace('DummyRepositoryContract', $this->feature.'RepositoryContract', $this->stub);
     }
