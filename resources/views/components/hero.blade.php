22c0e8e8b8f3beb8d29689230a6ce650586567c3
Jenny Porada <jennyingles@wayne.edu>
2018-09-14 13:24:32 -0400

    Removing a class

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -20,7 +20,7 @@
         @endforeach
     @else
         @foreach($images as $image)
-            <div class="w-full relative">
+            <div class="w-full">
                 <div class="pt-hero w-full bg-cover bg-top relative{{ $loop->first !== true ? ' lazy' : '' }}" @if($loop->first === true) style="background-image: url('{{ $image['relative_url'] }}')" @else data-src="{{ $image['relative_url'] }}"@endif></div>
             </div>
         @endforeach
