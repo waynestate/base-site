bfd55dfcf71ae1b4b37e66ca0b0ca0f7677952bf
Jenny Porada <jennyingles@wayne.edu>
2018-09-14 13:23:32 -0400

    Extended the bg-gradient by 5%, consolidated hero component

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -7,24 +7,15 @@
         @foreach($images as $image)
             <div class="w-full relative">
                 <div class="pt-hero w-full bg-cover bg-green-darkest md:bg-gradient-dark md:overflow-hidden bg-top relative{{ $loop->first !== true ? ' lazy' : '' }}" @if($loop->first === true) style="background-image: url('{{ $image['relative_url'] }}')" @else data-src="{{ $image['relative_url'] }}"@endif></div>
-
-                @if(in_array($page['controller'], config('base.hero_full_controllers')))
-                    <div class="md:absolute md:pin-b md:pin-x md:text-white md:text-shadow-dark @if(count($images) > 1) px-8 py-6 @else p-4 lg:p-10 @endif">
-                        <div class="row">
-                            <h1 class="leading-tight text-2xl xl:text-5xl">{{ $image['title'] }}</h1>
+                <div class="md:absolute md:pin-b md:pin-x md:text-white md:text-shadow-dark @if(count($images) > 1) p-6 @else py-4 @if(in_array($page['controller'], config('base.hero_full_controllers')))lg:pb-10 @endif @endif">
+                    <div class="row">
+                        <div class="mx-4">
+                            <h1 class="leading-tight text-2xl @if(in_array($page['controller'], config('base.hero_full_controllers')))xl:text-5xl @else xl:text-4xl @endif">{{ $image['title'] }}</h1>
                             @if(!empty($image['excerpt']))<p class="inline pr-2 md:text-lg">{{ $image['excerpt'] }}</p>@endif
                             @if(!empty($image['link']))<a href="{{ $image['link'] }}" class="underline font-bold hover:no-underline md:text-white md:text-lg">{{ config('base.hero_text_more') }}</a>@endif
                         </div>
                     </div>
-                @else
-                    <div class="md:absolute md:pin-b md:pin-x md:text-white md:text-shadow-dark @if(count($images) > 1) px-8 py-6 @else p-4 @endif">
-                        <div class="row">
-                            <h1 class="leading-tight text-2xl xl:text-4xl">{{ $image['title'] }}</h1>
-                            @if(!empty($image['excerpt']))<p class="inline pr-2">{{ $image['excerpt'] }}</p>@endif
-                            @if(!empty($image['link']))<a href="{{ $image['link'] }}" class="underline font-bold hover:no-underline md:text-white">{{ config('base.hero_text_more') }}</a>@endif
-                        </div>
-                    </div>
-                @endif
+                </div>
             </div>
         @endforeach
     @else
