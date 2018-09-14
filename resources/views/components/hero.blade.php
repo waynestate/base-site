00669fc8f7c8a55593cea6907c9c620509d0f827
Jenny Porada <jennyingles@wayne.edu>
2018-09-14 12:58:14 -0400

    Cleaned up some styles and added a drop shadow on the flickity arrows

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -2,16 +2,36 @@
     $images => array // ['relative_url']
 --}}
 
-<div class="mb-4 mt:mx-0{{ !empty($images) && count($images) > 1 ? ' rotate' : '' }}{{ !empty($show_site_menu) && $show_site_menu === true ? ' contained -mx-4' : ' full' }}">
-    @foreach($images as $image)
-        <div class="pt-partial w-full bg-cover bg-top relative{{ $loop->first !== true ? ' lazy' : '' }}" @if($loop->first === true) style="background-image: url('{{ $image['relative_url'] }}')" @else data-src="{{ $image['relative_url'] }}"@endif>
-            @if(!empty($image['excerpt']) && config('base.hero_text_enabled') && in_array($page['controller'], config('base.hero_text_controllers')))
-                <div class="bg-green-darker text-white py-4 lg:absolute pin-b pin-l pin-r">
-                    <div class="row px-4">
-                        {{ $image['excerpt'] }} @if(!empty($image['link'])) <a href="{{ $image['link'] }}" class="text-white underline font-bold pl-2 hover:no-underline">{{ config('base.hero_text_more') }}</a>@endif
+<div class="mb-4 mt:mx-0{{ !empty($images) && count($images) > 1 ? ' rotate' : '' }}{{ !empty($show_site_menu) && $show_site_menu === true ? ' contained -mx-4' : ' full' }} bg-grey-lighter md:bg-transparent">
+    @if(in_array($page['controller'], config('base.hero_text_controllers')))
+        @foreach($images as $image)
+            <div class="w-full relative">
+                <div class="pt-hero w-full bg-cover bg-green-darkest md:bg-gradient-dark md:overflow-hidden bg-top relative{{ $loop->first !== true ? ' lazy' : '' }}" @if($loop->first === true) style="background-image: url('{{ $image['relative_url'] }}')" @else data-src="{{ $image['relative_url'] }}"@endif></div>
+
+                @if(in_array($page['controller'], config('base.hero_full_controllers')))
+                    <div class="md:absolute md:pin-b md:pin-x md:text-white md:text-shadow-dark @if(count($images) > 1) px-8 py-6 @else p-4 lg:p-10 @endif">
+                        <div class="row">
+                            <h1 class="leading-tight text-2xl xl:text-5xl">{{ $image['title'] }}</h1>
+                            @if(!empty($image['excerpt']))<p class="inline pr-2 md:text-lg">{{ $image['excerpt'] }}</p>@endif
+                            @if(!empty($image['link']))<a href="{{ $image['link'] }}" class="underline font-bold hover:no-underline md:text-white md:text-lg">{{ config('base.hero_text_more') }}</a>@endif
+                        </div>
                     </div>
-                </div>
-            @endif
-        </div>
-    @endforeach
+                @else
+                    <div class="md:absolute md:pin-b md:pin-x md:text-white md:text-shadow-dark @if(count($images) > 1) px-8 py-6 @else p-4 @endif">
+                        <div class="row">
+                            <h1 class="leading-tight text-2xl xl:text-4xl">{{ $image['title'] }}</h1>
+                            @if(!empty($image['excerpt']))<p class="inline pr-2">{{ $image['excerpt'] }}</p>@endif
+                            @if(!empty($image['link']))<a href="{{ $image['link'] }}" class="underline font-bold hover:no-underline md:text-white">{{ config('base.hero_text_more') }}</a>@endif
+                        </div>
+                    </div>
+                @endif
+            </div>
+        @endforeach
+    @else
+        @foreach($images as $image)
+            <div class="w-full relative">
+                <div class="pt-hero w-full bg-cover bg-top relative{{ $loop->first !== true ? ' lazy' : '' }}" @if($loop->first === true) style="background-image: url('{{ $image['relative_url'] }}')" @else data-src="{{ $image['relative_url'] }}"@endif></div>
+            </div>
+        @endforeach
+    @endif
 </div>
