f484f5a04801c6424b0858e90ed75c67d53b1a95
Jenny Porada <jennyporada@gmail.com>
2018-10-18 18:00:58 -0400

    Formatting change on figure component

--- styleguide/Views/styleguide-figure.blade.php
+++ styleguide/Views/styleguide-figure.blade.php
@@ -20,8 +20,8 @@
         <pre class="fig-none hidden" style="background: #EAEAEA; margin-bottom: 10px; overflow: scroll;">
         {!! htmlspecialchars('
 <figure>
-<img src="https://placehold.it/400x300" alt="Placeholder">
-<figcaption>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</figcaption>
+    <img src="https://placehold.it/400x300" alt="Placeholder">
+    <figcaption>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</figcaption>
 </figure>
         ') !!}
         </pre>
