f7ddc4f0696fd98f84991725b5b1530e8bca265c
Chris Pelzer <chrispelzer@wayne.edu>
2019-01-09 10:29:12 -0500

    test deploy

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -4,7 +4,7 @@
     @include('components.page-title', ['title' => $page['title']])
 
     <div class="content">
-        {!! $page['content']['main'] !!}
+        1{!! $page['content']['main'] !!}
     </div>
 
     @if(!empty($news) || !empty($events))
