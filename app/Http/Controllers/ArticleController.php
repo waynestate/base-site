0b75492343dbe69fcc1f820bb87d6005a1c7f2cd
Robert Vrabel <rvrabel@gmail.com>
2019-01-15 14:31:10 -0500

    Remove uncessary comments

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -35,23 +35,18 @@ class ArticleController extends Controller
     {
         $topic['topic'] = null;
 
-        // Get the topic
         if(!empty($request->slug)) {
             $topic = $this->topic->find(null, $request->slug);
 
-            // 404 the page since the category doens't exist
             if(isset($topic['topic']['errors'])) {
                 return abort('404');
             }
         }
 
-        // Get the articles
         $articles = $this->article->listing([7], 2, $request->query('page'), !empty($topic['topic']['data']['id']) ? [$topic['topic']['data']['id']] : null);
 
-        // Get topics url
         $articles['topics_url'] = $this->article->topicsUrl();
 
-        // Disable hero images
         $request->data['hero'] = false;
 
         return view('articles', merge($request->data, $articles, $topic));
@@ -65,23 +60,18 @@ class ArticleController extends Controller
      */
     public function show(Request $request)
     {
-        // Get the news item
         $article = $this->article->find($request->id, [7]);
 
-        // If the news item does not exist or isn't published
         if (empty($article['article']['data']) || $article['article']['data']['status'] !== 'Published') {
             return abort('404');
         }
 
-        // Set the page title to the news item title
         $request->data['page']['title'] = $article['article']['data']['title'];
 
-        // Set hero
         if (!empty($article['article']['data']['hero_image']['url'])) {
             $request->data['hero'][]['relative_url'] = $article['article']['data']['hero_image']['url'];
         }
 
-        // Set the meta image information
         $request->data['meta']['image'] = $this->article->getImageUrl($article['article']['data']);
 
         return view('article', merge($request->data, $article));
