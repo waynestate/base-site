cb837f880a44a6c402a800396835baf456bdfb3f
Robert Vrabel <rvrabel@gmail.com>
2019-04-24 13:54:09 -0400

    Remove green background from featured promo title

--- resources/views/components/featured-promo.blade.php
+++ resources/views/components/featured-promo.blade.php
@@ -4,5 +4,5 @@
 
 @if(!empty($featured_promo['link']))<a href="{{ $featured_promo['link'] }}">@endif
     @image($featured_promo['relative_url'], $featured_promo['excerpt'], 'min-w-full mt-2')
-    <p class="font-bold hover:underline p-4 -mt-2 text-white bg-green">{{ $featured_promo['title'] }}</p>
+    <p class="underline">{{ $featured_promo['title'] }}</p>
 @if(!empty($featured_promo['link']))</a>@endif
