3e22d6fd9501489153369ec130abaa6a5fe0e661
Robert Vrabel <rvrabel@gmail.com>
2019-07-08 11:11:38 -0400

    Add aria-labelledby for svgs that have links around them.

--- resources/views/components/footer-social.blade.php
+++ resources/views/components/footer-social.blade.php
@@ -4,7 +4,7 @@
 <div class="bg-green-darker py-6">
     <div class="row flex justify-center items-center">
             @foreach($social as $item)
-                <a href="{{ $item['link'] }}" target="_blank" rel="noopener" class="flex justify-center items-center h-10 w-10 lg:h-14 lg:w-14 mx-1 sm:mx-2 md:mx-3 lg:mx-4 text-green-lightest fill-current bg-green rounded-full transition transition-property-bg transition-delay-none hover:bg-green-lighter hover:text-white">
+                <a href="{{ $item['link'] }}" target="_blank" aria-labelledby="{{ strtolower(trim($item['title'])) }}" rel="noopener" class="flex justify-center items-center h-10 w-10 lg:h-14 lg:w-14 mx-1 sm:mx-2 md:mx-3 lg:mx-4 text-green-lightest fill-current bg-green rounded-full transition transition-property-bg transition-delay-none hover:bg-green-lighter hover:text-white">
                     @svg($item['title'], 'w-6 h-6 lg:w-8 lg:h-8')
                 </a>
             @endforeach
