1f838e2b99446d66af03a5e53341c5009344be54
Robert Vrabel <rvrabel@gmail.com>
2019-07-09 11:41:58 -0400

    Move the visual date inside the loop and only show it on the first item of the loop. If the visual date isn't shown, the margin the event information over to visually line it up properly.

--- resources/views/components/events-listing.blade.php
+++ resources/views/components/events-listing.blade.php
@@ -8,15 +8,18 @@
 
 <ul>
     @foreach($events as $key => $dates)
-        <li class="flex -mx-2">
-            <div class="mx-2">
-                <div class="relative border-2 border-green rounded-sm text-center mb-4">
-                    <div class="w-12 bg-green text-white leading-none border-b-2 border-green text-sm">{{ apdatetime(date('M' , strtotime($key))) }}</div>
-                    <div class="text-green text-2xl leading-tight">{{ apdatetime(date('j' , strtotime($key))) }}</div>
-                </div>
-            </div>
-            <div class="mx-2 flex-grow">
-                @foreach($dates as $event)
+        @foreach($dates as $event)
+            <li class="flex -mx-2">
+                @if($loop->first)
+                    <div class="mx-2">
+                        <div class="relative border-2 border-green rounded-sm text-center mb-4">
+                            <div class="w-12 bg-green text-white leading-none border-b-2 border-green text-sm">{{ apdatetime(date('M' , strtotime($key))) }}</div>
+                            <div class="text-green text-2xl leading-tight">{{ apdatetime(date('j' , strtotime($key))) }}</div>
+                        </div>
+                    </div>
+                @endif
+
+                <div class="mx-2 flex-grow{{ ! $loop->first ? ' ml-19' :'' }}">
                     <div class="mb-2 pb-2 border-b border-solid border-grey-light">
                         <a class="block hover:underline" href="{{ $event['url'] }}">{{ $event['title'] }}
                             <span class="visually-hidden"> on {{ apdatetime(date('M d, Y' , strtotime($key))) }}
@@ -27,13 +30,13 @@
                             @if(!(bool)$event['is_all_day']){{ apdatetime(date('g:i a' , strtotime($event['start_time']))) }}@else All day @endif
                         </time>
                     </div>
-                @endforeach
-                @if($dates == end($events))
-                    <div>
-                        <a href="//events.wayne.edu/{{ $cal_name ?? 'main/' }}month/" class="hover:underline">{{ $link_text ?? 'More events' }}</a>
-                    </div>
-                @endif
-            </div>
-        </li>
+                </div>
+            </li>
+        @endforeach
+        @if($dates == end($events))
+            <li class="flex -mx-2 ml-17">
+                <a href="//events.wayne.edu/{{ $cal_name ?? 'main/' }}month/" class="hover:underline">{{ $link_text ?? 'More events' }}</a>
+            </li>
+        @endif
     @endforeach
 </ul>
