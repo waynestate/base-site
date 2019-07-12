4f4efb13348d7144d11ef76127b330f3373c00a7
Robert Vrabel <rvrabel@gmail.com>
2019-07-12 13:36:59 -0400

    Convert these to use the image helper

--- resources/views/grid.blade.php
+++ resources/views/grid.blade.php
@@ -13,7 +13,7 @@
                 <li class="w-full md:w-1/2 xl:w-1/3 px-4 pb-6">
                     <a href="{{ $item['link'] }}" class="flex md:block">
                         <div class="w-1/3 md:w-full">
-                            <img src="{{ $item['relative_url'] }}" alt="" role="presentation" />
+                            @image($item['relative_url'])
                         </div>
                         <div class="w-2/3 pl-4 md:pl-0">
                             <div class="font-bold hover:underline">{{ $item['title'] }}</div>
