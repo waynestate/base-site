7c960f821259bb8ed6228f4ba20e2d5229de5757
Robert Vrabel <rvrabel@gmail.com>
2019-08-08 14:03:23 -0400

    Use getBoundingClientRect to get the position of the element since it is more accurate Change accordion id so it is on the link so it can scroll to it since it is a visible element

--- resources/views/components/accordion.blade.php
+++ resources/views/components/accordion.blade.php
@@ -4,8 +4,8 @@
 <ul class="accordion">
     @foreach($items as $key=>$item)
         <li>
-            <a href="#definition-{{ $key }}"><span aria-hidden="true"></span>{{ $item['title'] }}</a>
-            <div class="content" id="definition-{{ $key }}">{!! $item['description'] !!}</div>
+            <a href="#definition-{{ $key }}" id="definition-{{ $key }}"><span aria-hidden="true"></span>{{ $item['title'] }}</a>
+            <div class="content">{!! $item['description'] !!}</div>
         </li>
     @endforeach
 </ul>
