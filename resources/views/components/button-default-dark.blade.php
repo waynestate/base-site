5f8bbc942b7365e2a5169fe7f616a1b70197dbe3
Robert Vrabel <rvrabel@gmail.com>
2019-08-20 08:42:59 -0400

    Remove this exception since it doesn't look like axe is reporting it anymore

--- resources/views/components/button-default-dark.blade.php
+++ resources/views/components/button-default-dark.blade.php
@@ -3,7 +3,7 @@
 --}}
 
 @if(!empty($button['excerpt']))
-    <a href="{{ $button['link'] }}" class="button-bg-image-dark button expanded bg-gradient-green text-white text-left">
+    <a href="{{ $button['link'] }}" class="button expanded bg-gradient-green text-white text-left">
         <div class="block text-lg xl:text-xl leading-tight">{{ $button['title'] }}</div>
         <div class="text-sm pb-1 leading-tight font-normal">{{ $button['excerpt'] }}</div>
     </a>
