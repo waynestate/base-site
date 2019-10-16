fcc1a1145c2749ecde4972d108554ae73018753e
Jenny Porada <ft8196@wayne.edu>
2019-10-16 16:36:09 -0400

    Print styles that were noticed when printing law pages

--- resources/views/components/menu-top.blade.php
+++ resources/views/components/menu-top.blade.php
@@ -2,16 +2,16 @@
     $site => array // ['short-title', 'title', 'subsite-folder']
     $top_menu_output => string // '<ul></ul>'
 --}}
-<div class="menu-top-container bg-green-dark">
+<div class="menu-top-container bg-green-dark print:bg-transparent">
     <div class="row flex">
         <div class="flex-grow mx-4 py-2" data-short-title="{{ $site['short-title'] }}">
             @if(config('base.surtitle') !== null && ($site['parent']['id'] === null && config('base.surtitle_main_site_enabled') === true) || ($site['parent']['id'] !== null && config('base.surtitle') !== null))
                 <h1 class="text-base mb-0 font-normal leading-tight">
-                    <a href="{{ config('base.surtitle_url') }}" class="text-white">{{ config('base.surtitle') }}</a>
+                    <a href="{{ config('base.surtitle_url') }}" class="text-white print:text-black">{{ config('base.surtitle') }}</a>
                 </h1>
 
                 <h2 class="font-normal mb-1 text-2xl">
-                    <a href="/{{ $site['subsite-folder'] !== null ? rtrim($site['subsite-folder'], '/') : '' }}" class="text-white">
+                    <a href="/{{ $site['subsite-folder'] !== null ? rtrim($site['subsite-folder'], '/') : '' }}" class="text-white print:text-black">
                         @if($site['short-title'] !== '')
                             <span class="mt:hidden">{{ $site['short-title'] }}</span>
                             <span class="hidden mt:inline">{{ $site['title'] }}</span>
@@ -22,7 +22,7 @@
                 </h2>
             @else
                 <h1 class="font-normal mb-0 text-2xl leading-none py-3">
-                    <a href="/{{ $site['subsite-folder'] !== null ? rtrim($site['subsite-folder'], '/') : '' }}" class="text-white">
+                    <a href="/{{ $site['subsite-folder'] !== null ? rtrim($site['subsite-folder'], '/') : '' }}" class="text-white print:text-black">
                         @if($site['short-title'] !== '')
                             <span class="mt:hidden">{{ $site['short-title'] }}</span>
                             <span class="hidden mt:inline">{{ $site['title'] }}</span>
@@ -43,7 +43,7 @@
         @endif
 
         <div class="flex flex-1 justify-end items-center mx-4 mt:hidden">
-            <button class="menu-toggle menu-icon text-white text-3xl mt:hidden" data-toggle="menu" aria-controls="menu" aria-label="Menu" tabindex="0"><span class="visually-hidden">Menu</span></button>
+            <button class="menu-toggle menu-icon text-white print:text-black text-3xl mt:hidden" data-toggle="menu" aria-controls="menu" aria-label="Menu" tabindex="0"><span class="visually-hidden">Menu</span></button>
         </div>
     </div>
 </div>
