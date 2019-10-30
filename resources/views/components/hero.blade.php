69fc3e73519836ff816fdde459d9a9a63246ff09
Jenny Porada <ft8196@wayne.edu>
2019-10-30 12:00:21 -0400

    Removing a line

--- resources/views/components/hero.blade.php
+++ resources/views/components/hero.blade.php
@@ -26,7 +26,6 @@
                             </div>
                         </div>
                     </div>
-
                 @endif
             @else
                 <div class="w-full relative">
