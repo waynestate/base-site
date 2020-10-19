e5ebe7f94130e35da8d2548b658b15e9e80cbc4b
Chuck Hutchins <chutchins@wayne.edu>
2020-10-19 16:22:19 -0400

    adding more comprehensive mobile styling to spotlight view

--- resources/views/spotlight-view.blade.php
+++ resources/views/spotlight-view.blade.php
@@ -2,16 +2,16 @@
 
 @section('content')
     <div class="row flex flex-wrap -mx-4">
-        <div class="w-full lg:w-1/3 px-4 mt-6 mb-4">
+        <div class="w-full md:w-1/3 px-4 mt-6">
             @if(!empty($spotlight['relative_url']))
-                @image($spotlight['relative_url'], $spotlight['filename_alt_text'], 'w-full')
+                @image($spotlight['relative_url'], $spotlight['filename_alt_text'], 'sm:h-64 md:h-auto mx-auto md:mx-0')
             @else
-                <div class="w-full pt-4/3 bg-cover bg-center" style="background-image: url('/_resources/images/no-photo.svg');"></div>
+                <img src="/_resources/images/no-photo.svg" alt="{{ $page['title'] }}" class="sm:h-64 md:h-auto block mx-auto md:mx-0">
             @endif
         </div>
 
-        <div class="w-full lg:w-2/3 px-4">
-            @include('components.page-title', ['title' => $page['title'], 'class' => 'hidden lg:block'])
+        <div class="w-full md:w-2/3 px-4">
+            @include('components.page-title', ['title' => $page['title']])
 
             <div class="content">
                 @if(!empty($spotlight['excerpt']))
