8c914b37f95ef043b1939a606a5dcb6d4b30ab4c
Chuck Hutchins <chutchins@wayne.edu>
2020-11-11 11:05:16 -0500

    adding directions to use template features

--- styleguide/Pages/ContactTables.php
+++ styleguide/Pages/ContactTables.php
@@ -17,7 +17,12 @@ class ContactTables extends Page
                 'controller' => 'ContactTableController',
                 'title' => 'Contact tables',
                 'id' => 101113,
-                'content' => [],
+                'content' => ['main' => '<p>In order to choose which groups show:</p>
+    <ol>
+        <li>Add a custom field named "profile_group_id"</li>
+        <li>Add in the IDs of the groups separated by the "|" character</li>
+        <li>Groups will be displayed in the order they are entered in the custom field</li>
+    </ol>'],
             ],
         ]);
     }
--- styleguide/Pages/DirectoryOrdered.php
+++ styleguide/Pages/DirectoryOrdered.php
@@ -10,13 +10,19 @@ class DirectoryOrdered extends Page
     public function getPageData()
     {
         return app('Factories\Page')->create(1, true, [
+            'data' => [
+                'profile_group_id' => '0|1',
+            ],
             'page' => [
                 'controller' => 'DirectoryController',
                 'title' => 'Directory ordered',
                 'id' => 101112,
-            ],
-            'data' => [
-                'profile_group_id' => '0|1',
+                'content' => ['main' => '<p>In order to choose which groups show:</p>
+    <ol>
+        <li>Add a custom field named "profile_group_id"</li>
+        <li>Add in the IDs of the groups separated by the "|" character</li>
+        <li>Groups will be displayed in the order they are entered in the custom field</li>
+    </ol>'],
             ],
         ]);
     }
