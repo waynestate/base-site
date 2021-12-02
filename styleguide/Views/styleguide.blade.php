00774df150fece8ba71905b9335f2a5868521970
Jenny Porada <jennyingles@gmail.com>
2021-12-02 11:16:09 -0500

    Adding into text from the content team to the beginning of the styleguide

--- styleguide/Views/styleguide.blade.php
+++ styleguide/Views/styleguide.blade.php
@@ -4,7 +4,15 @@
     @include('components.page-title', ['title' => $base['page']['title']])
 
     <div class="content">
-        <p>Welcome to your Styleguide!</p>
+        <p>Welcome to your style guide! This is essentially a how-to resource that will help you to determine what features you can 
+        incorporate into your website, and how to implement them. The style guide is specific to your site and provides you with the 
+        ability to customize your pages and visually enhance your content for a better user experience.</p>
+
+        <p>There are a variety of options available to use depending on your needs, but you can explore the menu to decide which items 
+        would fit best when adding or editing your pages. Some experience with basic html is beneficial and will help when incorporating 
+        some of the features into your site.</p>
+
+        <p>We do offer a <a href="//go.wayne.edu/webtraining">CMS self-paced web training</a> that covers how to edit and manage the content on your site. This training is required for university staff.</p>
 
         <h2>Basic text formatting</h2>
         <p class="mb-1">These HTML classes are available for formatting paragraph or span elements. Add <code>class="text-lg"</code>, replacing "text-lg" with any of the names in the list below.</p>
