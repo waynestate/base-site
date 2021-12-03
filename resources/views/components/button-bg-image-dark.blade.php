da35c7d7e0aa4725e629f4ff422b1212f1307258
Jenny Porada <jennyingles@gmail.com>
2021-12-03 17:55:45 -0500

    Consolidated button code

--- resources/views/components/button-bg-image-dark.blade.php
+++ resources/views/components/button-bg-image-dark.blade.php
@@ -3,11 +3,15 @@
 --}}
 
 @if(!empty($button['link']) && !empty($button['relative_url']))
-    <a href="{{ $button['link'] }}" class="block relative rounded bg-green-900 overflow-hidden {{ $class ?? '' }}">
-        <img src="{{ $button['relative_url'] }}" alt="{{ $button['filename_alt_text'] }}">
-        <div class="absolute inset-0 bg-green-900 opacity-75"></div>
-        <div class="absolute inset-0 p-4 flex items-center">
-            <div class="w-full text-lg xl:text-xl font-bold text-white leading-tight text-center">{{ $button['title'] }}</div>
+    <a href="{{ $button['link'] }}" class="group block relative rounded bg-green-900 overflow-hidden {{ $class ?? '' }}">
+        @if(!empty($button['relative_url'])) @image($button['relative_url'], $button['filename_alt_text']) @endif
+        <div class="absolute inset-0 bg-green-900 opacity-75 group-hover:opacity-90 transition-all"></div>
+        <div class="absolute inset-0 flex items-center">
+            @if(!empty($button['secondary_relative_url']))
+                @image($button['secondary_relative_url'], $button['secondary_alt_text'])
+            @else
+                <div class="w-full text-lg xl:text-xl font-bold text-white leading-tight text-center px-4">{{ $button['title'] }}</div>
+            @endif
         </div>
     </a>
 @endif
--- styleguide/Views/styleguide-under-menu.blade.php
+++ styleguide/Views/styleguide-under-menu.blade.php
@@ -4,11 +4,12 @@
     @include('components.page-title', ['title' => $base['page']['title']])
 
     <p>Under menu buttons are available through promotion groups. You may not mix button types on a single page.</p>
-    <p>At the bottom of the menu on the left, you can see the standard button in use.</p>
+    <p>At the bottom of the menu on the left, you can see the default button in use.</p>
+    <p>When setting up the promo group, use this list of options: <code>Default, Green, Gold, Bg image dark, Bg image light</code>
 
     @if(!empty($buttons))
         <h2 class="mb-4">Default buttons</h2>
-        <div class="pb-4 2xl:flex items-start">
+        <div class="pb-4 md:flex items-start">
             <div class="md:mr-4 flex-shrink-0">
                 @include('components.button-default', ['button' => $buttons['default'], 'class' => 'w-72'])
             </div>
@@ -21,7 +22,7 @@
             </div>
         </div>
 
-        <div class="pb-4 2xl:flex items-start">
+        <div class="pb-4 md:flex items-start">
             <div class="md:mr-4 flex-shrink-0">
                 @include('components.button-default', ['button' => $buttons['default_two_lines'], 'class' => 'w-72'])
             </div>
@@ -35,7 +36,7 @@
             </div>
         </div>
 
-        <div class="pb-4 2xl:flex items-start">
+        <div class="pb-4 md:flex items-start">
             <div class="md:mr-4 flex-shrink-0">
                 @include('components.button-default', ['button' => $buttons['default_icon'], 'class' => 'w-72'])
             </div>
@@ -44,7 +45,7 @@
                 <ul class="ml-6 list-disc text-sm">
                     <li>Title</li>
                     <li>Excerpt</li>
-                    <li>Filename: Green SVG 40x40</li>
+                    <li>Filename: Green-800 SVG 40x40</li>
                     <li>Option: None or 'Default'</li>
                 </ul>
             </div>
@@ -53,7 +54,7 @@
         <hr />
         
         <h2 class="mb-4">Green buttons</h2>
-        <div class="pb-4 2xl:flex items-start">
+        <div class="pb-4 md:flex items-start">
             <div class="md:mr-4 flex-shrink-0">
                 @include('components.button-green', ['button' => $buttons['green'], 'class' => 'w-72'])
             </div>
@@ -66,7 +67,7 @@
             </div>
         </div>
 
-        <div class="pb-4 2xl:flex items-start">
+        <div class="pb-4 md:flex items-start">
             <div class="md:mr-4 flex-shrink-0">
                 @include('components.button-green', ['button' => $buttons['green_two_lines'], 'class' => 'w-72'])
             </div>
@@ -80,7 +81,7 @@
             </div>
         </div>
 
-        <div class="pb-4 2xl:flex items-start">
+        <div class="pb-4 md:flex items-start">
             <div class="md:mr-4 flex-shrink-0">
                 @include('components.button-green', ['button' => $buttons['green_icon'], 'class' => 'w-72'])
             </div>
@@ -97,8 +98,53 @@
 
         <hr />
 
+        <h2 class="mb-4">Gold buttons</h2>
+        <div class="pb-4 md:flex items-start">
+            <div class="md:mr-4 flex-shrink-0">
+                @include('components.button-gold', ['button' => $buttons['default'], 'class' => 'w-72'])
+            </div>
+            <div>
+                <h3 class="text-sm ">Promo setup:</h3>
+                <ul class="ml-6 list-disc text-sm">
+                    <li>Title</li>
+                    <li>Option: Gold</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="pb-4 md:flex items-start">
+            <div class="md:mr-4 flex-shrink-0">
+                @include('components.button-gold', ['button' => $buttons['default_two_lines'], 'class' => 'w-72'])
+            </div>
+            <div>
+                <h3 class="text-sm ">Promo setup:</h3>
+                <ul class="ml-6 list-disc text-sm">
+                    <li>Title</li>
+                    <li>Excerpt</li>
+                    <li>Option: Gold</li>
+                </ul>
+            </div>
+        </div>
+
+        <div class="pb-4 md:flex items-start">
+            <div class="md:mr-4 flex-shrink-0">
+                @include('components.button-gold', ['button' => $buttons['default_icon'], 'class' => 'w-72'])
+            </div>
+            <div>
+                <h3 class="text-sm ">Promo setup:</h3>
+                <ul class="ml-6 list-disc text-sm">
+                    <li>Title</li>
+                    <li>Excerpt</li>
+                    <li>Filename: Green-800 SVG 40x40</li>
+                    <li>Option: Gold</li>
+                </ul>
+            </div>
+        </div>
+
+        <hr />
+
         <h2 class="mb-4">Image buttons</h2>
-        <div class="pb-4 2xl:flex items-start">
+        <div class="pb-4 md:flex items-start">
             <div class="md:mr-4 flex-shrink-0">
                 @include('components.button-bg-image-light', ['button' => $buttons['bg_image_light'], 'class' => 'w-72'])
             </div>
@@ -113,7 +159,7 @@
             </div>
         </div>
 
-        <div class="pb-4 2xl:flex items-start">
+        <div class="pb-4 md:flex items-start">
             <div class="md:mr-4 flex-shrink-0">
                 @include('components.button-bg-image-dark', ['button' => $buttons['bg_image_dark'], 'class' => 'w-72'])
             </div>
@@ -131,9 +177,9 @@
         <hr />
 
         <h2 class="mb-4">SVG buttons</h2>
-        <div class="pb-4 2xl:flex items-start">
+        <div class="pb-4 md:flex items-start">
             <div class="md:mr-4 flex-shrink-0">
-                @include('components.button-svg-light-bg', ['button' => $buttons['svg_light_bg'], 'class' => 'w-72'])
+                @include('components.button-bg-image-light', ['button' => $buttons['svg_light_bg'], 'class' => 'w-72'])
             </div>
             <div>
                 <h3 class="text-sm ">Promo setup:</h3>
@@ -142,14 +188,14 @@
                     <li>Link</li>
                     <li>Filename: 600x218 jpg, Ratio 2.76:1</li>
                     <li>Secondary Image: 600x218 svg, Ratio 2.76:1</li>
-                    <li>Option: SVG light bg</li>
+                    <li>Option: Bg image light</li>
                 </ul>
             </div>
         </div>
 
-        <div class="pb-4 2xl:flex items-start">
+        <div class="pb-4 md:flex items-start">
             <div class="md:mr-4 flex-shrink-0">
-                @include('components.button-svg-dark-bg', ['button' => $buttons['svg_dark_bg'], 'class' => 'w-72'])
+                @include('components.button-bg-image-dark', ['button' => $buttons['svg_dark_bg'], 'class' => 'w-72'])
             </div>
             <div>
                 <h3 class="text-sm ">Promo setup:</h3>
@@ -158,7 +204,7 @@
                     <li>Link</li>
                     <li>Filename: 600x218 jpg, Ratio 2.76:1</li>
                     <li>Secondary Image: 600x218 svg, Ratio 2.76:1</li>
-                    <li>Option: SVG dark bg</li>
+                    <li>Option: Bg image dark</li>
                 </ul>
             </div>
         </div>
