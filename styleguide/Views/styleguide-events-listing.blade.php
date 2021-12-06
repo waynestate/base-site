459a66622040fa4f860af13596726e39bcb7a6e7
Jenny Porada <jennyingles@gmail.com>
2021-12-06 17:11:27 -0500

    Added events as a whole row in 2 columns

--- styleguide/Views/styleguide-events-listing.blade.php
+++ styleguide/Views/styleguide-events-listing.blade.php
@@ -3,11 +3,19 @@
 @section('content')
     @include('components.page-title', ['title' => $base['page']['title']])
 
-    <div class="row">
-        <div class="w-full md:w-1/2">
-            @if(!empty($events))
-                @include('components.events-listing', ['events' => $events])
-            @endif
-        </div>
+    <p>Events can be a half-sized column usually placed next to news, or take up the whole row in two columns.</p>
+
+    <div class="w-full md:w-1/2">
+        <h2>Events standard listing</h2>
+        @if(!empty($events))
+            @include('components.events-listing', ['events' => $events])
+        @endif
+    </div>
+
+    <div class="w-full mt-8">
+        <h2>Events in two columns</h2>
+        @if(!empty($events))
+            @include('components.events-listing-full-width', ['events' => $events, 'heading'=> 'bg-gold'])
+        @endif
     </div>
 @endsection
