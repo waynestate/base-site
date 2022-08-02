6c4ff82ac5be5c906efdc9e692906bf0187d853e
Ambyr Elan Lantigua <ambyrelan@wayne.edu>
2022-08-02 11:00:18 -0400

    include paginate component & variable

--- resources/views/articles.blade.php
+++ resources/views/articles.blade.php
@@ -3,9 +3,9 @@
 @section('content')
     @include('components.page-title', ['title' => $base['page']['title']])
 
-    @if(!empty($articles['data']))
+    @if(!empty($paginate->items()))
         <ul>
-            @foreach($articles['data'] as $article)
+            @foreach($paginate->items() as $article)
                 <li class="mb-3 pb-4 border-b border-gray-200">
                     <a href="{{ $article['link'] }}" class="font-bold text-lg block">
                         {{ $article['title'] }}
@@ -14,28 +14,10 @@
                 </li>
             @endforeach
         </ul>
+
+        @include('components.paginator')
     @else
         <p>Currently there are no articles{{ !empty($topic['data']['name']) ? ' for the category ' . strtolower($topic['data']['name']) : '' }}.</p>
-    @endif
-
-    @if(!empty($articles['meta']['prev_page_url']) || !empty($articles['meta']['next_page_url']))
-        <div class="row flex -mx-4">
-            <div class="w-1/2 px-4">
-                @if(!empty($articles['meta']['prev_page_url']))
-                    <p>
-                        <a href="{{ $articles['meta']['prev_page_url'] }}" class="button">&larr; Previous</a>
-                    </p>
-                @endif
-            </div>
-
-            <div class="w-1/2 px-4 text-right">
-                @if(!empty($articles['meta']['next_page_url']) && $articles['meta']['current_page'] !== 1)
-                    <p>
-                        <a href="{{ $articles['meta']['next_page_url'] }}" class="button">Next &rarr;</a>
-                    </p>
-                @endif
-            </div>
-        </div>
     @endif
 @endsection
 
