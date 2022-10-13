6ff813528c35586696db1a880eb4a8793bd7f335
Trace Remick <trace@wayne.edu>
2022-10-13 10:53:11 -0400

    Added surtitle to the title tag of the page.

--- resources/views/components/head-title.blade.php
+++ resources/views/components/head-title.blade.php
@@ -1,1 +1,1 @@
-@if($base['page']['title'] != null && $base['server']['path'] != '/' && $base['server']['path'] != rtrim($base['site']['subsite-folder'], '/')) {!! $base['page']['title'] !!} -@endif {!! $base['site']['title'] !!} - Wayne State University
+@if($base['page']['title'] != null && $base['server']['path'] != '/' && $base['server']['path'] != rtrim($base['site']['subsite-folder'], '/')) {!! $base['page']['title'] !!} -@endif {!! $base['site']['title'] !!}@if(!empty(config('base.surtitle'))) - {{ config('base.surtitle') }}@endif - Wayne State University
