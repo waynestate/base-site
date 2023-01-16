da50025d9626e96bd4a3fdd4f10bf1dfbe0e2441
Nick DeNardis <nick.denardis@gmail.com>
2023-01-16 18:25:32 -0500

    Move from PHP-CS-Fixer to Laravel Pint

--- php-cs-fixer-script.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-[[ $(grep "APP_ENV=local" .env) && ! -d "tools/php-cs-fixer" ]] && mkdir -p -m 0770 tools/php-cs-fixer
-[[ -d "tools/php-cs-fixer" ]] && composer require --with-all-dependencies --working-dir=tools/php-cs-fixer friendsofphp/php-cs-fixer
-
-exit 0
