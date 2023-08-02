6f8e504dfc5c4dfae5a40df374d4fdcd932f827d
Jenny Porada <jennyingles@gmail.com>
2023-08-02 15:31:23 -0400

    Extreme CSS hyperfocus automatically applying the external menu icon in most common cases

--- styleguide/Pages/MenuExternal.php
+++ styleguide/Pages/MenuExternal.php
@@ -14,15 +14,15 @@ class MenuExternal extends Page
         return app(PageFactory::class)->create(1, true, [
             'page' => [
                 'controller' => 'MenuTopController',
-                'title' => 'Menu external',
+                'title' => 'External menu icon',
                 'id' => 103100102,
                 'content' => [
                     'main' => '
-                        <h2>Setup</h2>
-                        <ul>
-                            <li>Add class "external" to menu links and under-menu items</li>
-                            <li>Suggestion: Use full url for any link considered external.</li>
-                        </ul>
+<p>Links within the <code>content</code>, <code>main-menu</code> and <code>under-menu</code> classes that contain https://, http://, and // will automatically have an external link icon applied. For an example, this is what it looks like to link to <a href="//wayne.edu">Wayne.edu</a>. This applies to text links as well as buttons. Buttons and links outside of the conditions above will not be modified.</p>
+<p>
+<a href="#//" class="button mr-4">Button within content class</a>
+<a href="#//" class="green-button">Button within content class</a><br />
+</p>
                     ',
                 ],
             ],
--- styleguide/Views/styleguide.blade.php
+++ styleguide/Views/styleguide.blade.php
@@ -6,13 +6,13 @@
     <div class="content">
         <p>The style guide is specific to each site, so please be sure you are viewing the correct one at <strong>[yourdomain].wayne.edu/styleguide</strong> (ex: <a href="//mac.wayne.edu/styleguide">mac.wayne.edu/styleguide</a>).
 
-        <p>Your style guide is a how-to resource that will help you to determine what features you can incorporate into your website and how to implement them. It provides you with the ability to customize your pages and visually enhance your content for a better user experience.</p>
+        <p>Your style guide is a how-to resource that will help you to determine what features you can incorporate into your website and how to implement them. It provides you with the ability to customize your pages and visually enhance your content for a better user <a class="external-link">experience</a>.</p>
 
         <p>There are a variety of options available to use depending on your needs, but you can explore the menu to decide which items 
         would fit best when adding or editing your pages. Some experience with basic html is beneficial and will help when incorporating 
         some of the features into your site.</p>
 
-        <p>We do offer a <a href="//go.wayne.edu/webtraining">CMS self-paced web training</a> that covers how to edit and manage the content on your site. This training is required for university staff.</p>
+        <p>We do offer a <a href="https://go.wayne.edu/webtraining">CMS self-paced web training</a> that covers how to edit and manage the content on your site. This training is required for university staff.</p>
         
         <h2>Basic text formatting</h2>
         <p class="mb-1">These HTML classes are available for formatting paragraph or span elements. Add <code>class="text-lg"</code>, replacing "text-lg" with any of the names in the list below.</p>
