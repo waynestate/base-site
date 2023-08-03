cc76aa5f75f45c2f0b9450137f2d061541b30699
Jenny Porada <jennyingles@gmail.com>
2023-08-02 22:52:44 -0400

    Reversing automatically applying the external link icon

--- styleguide/Pages/MenuExternal.php
+++ styleguide/Pages/MenuExternal.php
@@ -18,10 +18,13 @@ class MenuExternal extends Page
                 'id' => 103100102,
                 'content' => [
                     'main' => '
-<p>Links within the <code>content</code>, <code>main-menu</code> and <code>under-menu</code> classes that contain https://, http://, and // will automatically have an external link icon applied. For an example, this is what it looks like to link to <a href="//wayne.edu">Wayne.edu</a>. This applies to text links as well as buttons. Buttons and links outside of the conditions above will not be modified.</p>
+<p>Links and buttons given the <code>external-link</code> class within the <code>content</code>, <code>main-menu</code> and <code>under-menu</code> sections will display an external link icon.</p>
+<p>The icon color will match the text color of the link or button. A screen reader will read (external link) after the link.</p>
+<p>This is what it looks like to link to <a href="//wayne.edu" class="external-link">Wayne.edu</a> within a sentence.</p>
+<p>Below is an example of two common buttons with the class applied:</p>
 <p>
-<a href="#//" class="button mr-4">Button within content class</a>
-<a href="#//" class="green-button">Button within content class</a><br />
+<a href="#" class="inline-block button mr-4 external-link">External link button</a>
+<a href="#" class="green-button external-link">Green external link button</a><br />
 </p>
                     ',
                 ],
--- styleguide/Views/styleguide.blade.php
+++ styleguide/Views/styleguide.blade.php
@@ -12,7 +12,7 @@
         would fit best when adding or editing your pages. Some experience with basic html is beneficial and will help when incorporating 
         some of the features into your site.</p>
 
-        <p>We do offer a <a href="//go.wayne.edu/webtraining">CMS self-paced web training</a> that covers how to edit and manage the content on your site. This training is required for university staff.</p>
+        <p>We do offer a <a href="//go.wayne.edu/webtraining" class="external-link">CMS self-paced web training</a> that covers how to edit and manage the content on your site. This training is required for university staff.</p>
         
         <h2>Basic text formatting</h2>
         <p class="mb-1">These HTML classes are available for formatting paragraph or span elements. Add <code>class="text-lg"</code>, replacing "text-lg" with any of the names in the list below.</p>
