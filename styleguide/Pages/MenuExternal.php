29db5fc527107e9a6a72833fb1dde58b66be1e84
Jenny Porada <jennyingles@gmail.com>
2023-08-03 11:56:15 -0400

    Changing the class name from external-link to external

--- styleguide/Pages/MenuExternal.php
+++ styleguide/Pages/MenuExternal.php
@@ -18,13 +18,13 @@ class MenuExternal extends Page
                 'id' => 103100102,
                 'content' => [
                     'main' => '
-<p>Links and buttons given the <code>external-link</code> class within the <code>content</code>, <code>main-menu</code> and <code>under-menu</code> sections will display an external link icon.</p>
+<p>Links and buttons given the <code>external</code> class within the <code>content</code>, <code>main-menu</code> and <code>under-menu</code> sections will display an external link icon.</p>
 <p>The icon color will match the text color of the link or button. A screen reader will read (external link) after the link.</p>
-<p>This is what it looks like to link to <a href="//wayne.edu" class="external-link">Wayne.edu</a> within a sentence.</p>
+<p>This is what it looks like to link to <a href="//wayne.edu" class="external">Wayne.edu</a> within a sentence.</p>
 <p>Below is an example of two common buttons with the class applied:</p>
 <p>
-<a href="#" class="inline-block button mr-4 external-link">External link button</a>
-<a href="#" class="green-button external-link">Green external link button</a><br />
+<a href="#" class="inline-block button mr-4 external">External link button</a><br />
+<a href="#" class="green-button text-lg external">Green external link button</a>
 </p>
                     ',
                 ],
