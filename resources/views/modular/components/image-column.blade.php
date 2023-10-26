0bcfc4f6ec1d24a4aa057300a4d0031ac00acec1
Chris Pelzer <chrispelzer@gmail.com>
2023-10-26 10:27:49 -0400

    remove promo as it is redudant

--- resources/views/modular/components/image-column.blade.php
+++ resources/views/modular/components/image-column.blade.php
@@ -8,18 +8,18 @@
     </div>
 @endif
 
-@foreach($data as $image_promo)
+@foreach($data as $image)
     <div class="col-span-2 md:col-span-1">
         @if(!empty($component['heading']))
             <h2 class="mt-0">{{ $component['heading'] }}</h2>
         @endif
         <div class="grid gap-4">
             <div class="bg-green-800">
-                <a href="{{ $image_promo['link'] }}" class="relative block group">
-                    @image($image_promo['filename_url'], $image_promo['filename_alt_text'], 'w-full')
+                <a href="{{ $image['link'] }}" class="relative block group">
+                    @image($image['filename_url'], $image['filename_alt_text'], 'w-full')
                     <div class="bg-gradient-darkest absolute inset-x-0 bottom-0">
                         <div class="w-full text-white font-bold relative text-xl p-4 pb-3 pt-14 group-hover:underline group-focus:underline">
-                            {{ $image_promo['title'] }}
+                            {{ $image['title'] }}
                         </div>
                     </div>
                 </a>
