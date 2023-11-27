10509e481443e4b570cf8167b7d61e7e5514f54f
Nick DeNardis <nick.denardis@gmail.com>
2023-11-27 11:52:01 -0500

    Proof of concept page title limit to 60-70 chars

--- resources/views/components/head-title.blade.php
+++ resources/views/components/head-title.blade.php
@@ -1,1 +1,27 @@
-@if($base['page']['title'] != null && $base['server']['path'] != '/' && $base['server']['path'] != rtrim($base['site']['subsite-folder'], '/')) {!! $base['page']['title'] !!} -@endif {!! $base['site']['title'] !!}@if((config('base.surtitle') !== null && ($base['site']['parent']['id'] === null && config('base.surtitle_main_site_enabled') === true) || ($base['site']['parent']['id'] !== null && config('base.surtitle') !== null)) && !config('base.global.sites.' . $base['site']['id'] . '.surtitle_disabled')) - {{ config('base.surtitle') }}@endif - Wayne State University
+@php
+    $titles = [];
+    $char_length = 0;
+
+    // Page title (except homepages)
+    if (!empty($base['page']['title']) 
+        && $base['server']['path'] != '/' 
+        && $base['server']['path'] != rtrim($base['site']['subsite-folder'], '/')
+    ) {
+        $titles[] = $base['page']['title'];
+        $char_length += strlen($base['page']['title']);
+    }
+
+    // Site title (if there is enough room)
+    if (($char_length + 22) < 60) {
+        $titles[] = $base['site']['title'];
+        $char_length += strlen($base['site']['title']);
+    }
+
+    // University name (if there is room)
+    if (($char_length + 22) <= 70) {
+        $titles[] = 'Wayne State University';
+    }
+
+    // Display with dash separators
+    echo implode(' - ', $titles);
+@endphp
