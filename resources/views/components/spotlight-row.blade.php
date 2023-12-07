4d31dc45f9abd2980e10ff076ad2cd694e430412
Jenny Porada <jennyingles@gmail.com>
2023-12-07 12:57:07 -0500

    Adding spotlight column

--- resources/views/components/spotlight-row.blade.php
+++ resources/views/components/spotlight-row.blade.php
@@ -3,11 +3,11 @@
 --}}
 @foreach($data as $item)
     <{{ !empty($item['link']) ? 'a href='.$item['link'] : 'div' }} class="{{ !empty($item['link']) ? 'group' : '' }}">
-        <blockquote class="relative flex border-0 m-0 p-0 text-black">
+        <blockquote class="flex gap-x-6 border-0 m-0 p-0 text-black">
             <div class="flex flex-wrap content-center w-full">
                 <div class="content w-full text-lg md:text-xl lg:text-xl lg:leading-relaxed">{!! $item['description'] !!}</div>
                 <div class="w-full flex items-center lg:items-start gap-x-2 mb-4 lg:mb-2">
-                    <div class="w-24 lg:w-1/4 lg:absolute top-0 right-0 shrink-0">
+                    <div class="w-20 lg:w-1/4 lg:absolute top-0 right-0 shrink-0">
                         <div class="lg:hidden rounded-full overflow-hidden w-full pt-full border relative">
                             @image($item['filename_url'], $item['filename_alt_text'], 'block inset-0 absolute z-10 w-full h-full object-cover')
                         </div>
--- styleguide/Http/Controllers/SinglePromoController.php
+++ styleguide/Http/Controllers/SinglePromoController.php
@@ -81,7 +81,7 @@ class SinglePromoController extends Controller
                     'link' => '#',
                 ]),
                 'component' => [
-                    'heading' => 'Promo row heading',
+                    'heading' => 'Promo row',
                     'filename' => 'promo-row',
                 ],
             ],
