0d0a1da94989411a24ecae1c4d29e02a6617ff75
Jenny Porada <jennyingles@gmail.com>
2024-02-01 15:35:47 -0500

    Updating color hex codes for gray

--- styleguide/Repositories/ModularPageRepository.php
+++ styleguide/Repositories/ModularPageRepository.php
@@ -3,8 +3,6 @@
 namespace Styleguide\Repositories;
 
 use App\Repositories\ModularPageRepository as Repository;
-use Factories\GenericPromo;
-use Factories\HeroImage;
 
 class ModularPageRepository extends Repository
 {
@@ -15,25 +13,6 @@ class ModularPageRepository extends Repository
     public function getModularComponents(array $data): array
     {
         $components = [];
-        $components = [
-            'hero-1' => [
-                'data' => app(HeroImage::class)->create(1, false),
-                'component' => [
-                    'filename' => 'hero',
-                ],
-            ],
-            'catalog-1' => [
-                'data' => app(GenericPromo::class)->create(4, false, [
-                    'description' => '',
-                ]),
-                'component' => [
-                    'heading' => 'Four column catalog',
-                    'filename' => 'catalog',
-                    'columns' => '4',
-                    'showDescription' => false,
-                ],
-            ],
-        ];
 
         return $components;
     }
