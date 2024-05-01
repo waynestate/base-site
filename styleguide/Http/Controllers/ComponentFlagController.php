adcad23958bfa74b73f4ffdbbd25597a5f4c9992
Jenny Porada <jennyingles@gmail.com>
2024-05-01 14:17:55 -0400

    feat: Moving and updating the flag back into the components menu

--- styleguide/Http/Controllers/ComponentFlagController.php
+++ styleguide/Http/Controllers/ComponentFlagController.php
@@ -14,8 +14,35 @@ class ComponentFlagController extends Controller
      */
     public function index(Request $request): View
     {
+        $request->data['base']['page']['content']['main'] = '
+<p>Flag is a global component that cannot be enabled by page field.</p>
+';
+
+        $components = [
+            'accordion' => [
+                'data' => [
+                    0 => [
+                        'promo_item_id' => 'promoGroupDetails',
+                        'title' => 'Promo group details',
+                        'description' => '',
+                        'table' => [
+                            'Title' => 'Smaller upercase text',
+                            'Excerpt' => 'Larger italicized text',
+                            'Link' => 'URL',
+                        ],
+                    ],
+                ],
+                'component' => [
+                    'filename' => 'accordion-styleguide',
+                ],
+            ],
+        ];
+
+        // Assign components globally
+        $request->data['base']['components'] = $components;
+
         $request->data['base']['flag'] = app(Flag::class)->create(1, true);
 
-        return view('styleguide-childpage', merge($request->data));
+        return view('childpage', merge($request->data));
     }
 }
