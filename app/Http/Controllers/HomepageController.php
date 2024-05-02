924de479002616562902474da7fa8b3b7e5db152
Ambyr <ambyrelan@wayne.edu>
2024-05-02 14:28:49 -0400

    fix: remove unnecessary code

--- app/Http/Controllers/HomepageController.php
+++ app/Http/Controllers/HomepageController.php
@@ -10,43 +10,6 @@ namespace App\Http\Controllers;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
 use Contracts\Repositories\HomepageRepositoryContract;
-use Contracts\Repositories\EventRepositoryContract;
-use Contracts\Repositories\ArticleRepositoryContract;
-
-class HomepageController extends Controller
-{
-    /**
-     * Construct the controller.
-     */
-    public function __construct(HomepageRepositoryContract $promo, ArticleRepositoryContract $article, EventRepositoryContract $event)
-    {
-        $this->promo = $promo;
-        $this->article = $article;
-        $this->event = $event;
-    }
-
-    /**
-     * Display the homepage view.
-     */
-    public function index(Request $request): View
-    {
-        // $request->data['base']['show_site_menu'] = false;
-        // $promos = $this->promo->getHomepagePromos();
-        
-        $components = 
-
-<?php
-/*
-* Status: Private
-* Description: Homepage Template
-* Default: false
-*/
-
-namespace App\Http\Controllers;
-
-use Illuminate\View\View;
-use Illuminate\Http\Request;
-use Contracts\Repositories\HomepageRepositoryContract;
 use Contracts\Repositories\ModularPageRepositoryContract;
 use Contracts\Repositories\EventRepositoryContract;
 use Contracts\Repositories\ArticleRepositoryContract;
@@ -57,9 +20,9 @@ class HomepageController extends Controller
      * Construct the controller.
      */
     public function __construct(
-        HomepageRepositoryContract $promo, 
+        HomepageRepositoryContract $promo,
         ModularPageRepositoryContract $modularComponent,
-        ArticleRepositoryContract $article, 
+        ArticleRepositoryContract $article,
         EventRepositoryContract $event
     ) {
         $this->promo = $promo;
@@ -78,10 +41,10 @@ class HomepageController extends Controller
         $promos = $this->promo->getHomepagePromos($request->data);
 
         $modularComponents['modularComponents'] = [];
-        
+
         if(!empty($request->data['base']['data'])) {
             $modularComponents['modularComponents'] = $this->modularComponent->getModularComponents($request->data['base']);
-            $promos['components'] = $modularComponents['modularComponents']; 
+            $promos['components'] = $modularComponents['modularComponents'];
         }
 
         $articles = $this->article->listing($request->data['base']['site']['news']['application_id']);
