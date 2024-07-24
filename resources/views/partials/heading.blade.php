d88bcd0d5dcb145f1de28c127a22b611ba26a8be
Jenny Porada <jennyingles@gmail.com>
2024-07-24 14:50:46 -0400

    Moving heading logic into modular repository

--- resources/views/partials/heading.blade.php
+++ resources/views/partials/heading.blade.php
@@ -1,8 +1,9 @@
+
 {{--
     "heading":"Heading text",
     "headingLevel":["h2", "h3", "h4"],
     "headingClass":"text-green gold-divider"
 --}}
-<{{ !empty($headingLevel) && strtolower($headingLevel) != 'h1' ? $headingLevel : 'h2' }} id="{{ Str::slug($heading) }}" class="{{ $headingClass ?? '' }}">
+<{{ $headingLevel ?? 'h2' }} id="{{ Str::slug($heading) }}" class="{{ $headingClass ?? '' }}">
     {!! strip_tags($heading, ['em', 'strong']) !!}
-</{{ !empty($headingLevel) && strtolower($headingLevel) != 'h1' ? $headingLevel : 'h2' }}>
+</{{ $headingLevel ?? 'h2' }}>
