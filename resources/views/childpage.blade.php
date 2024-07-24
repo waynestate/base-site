469ae3afb0eeed4623d91e8f2955916038cf1d7a
Jenny Porada <jennyingles@gmail.com>
2024-07-24 14:46:12 -0400

    passing in row/padding into page title partial when the controller is fullwidthcontroller

--- resources/views/childpage.blade.php
+++ resources/views/childpage.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.' . (!empty($base['layout']) ? $base['layout'] : 'main'))
     
 @section('content')
-    @include('components.page-title', ['title' => $base['page']['title']])
+    @include('components.page-title', ['title' => $base['page']['title'], 'class' => in_array($base['page']['controller'], config('base.full_width_controllers')) ? 'row px-4' : '' ])
 
     @if(empty($base['components']['page-content-row']) && empty($base['components']['page-content-column']))
         <div class="content">
