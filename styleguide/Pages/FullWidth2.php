f18b28f9dc22b549b062aaee315876f996878978
Jenny Porada <jennyingles@gmail.com>
2024-12-11 15:44:02 -0500

    Enabling full width template in the main app

--- styleguide/Pages/FullWidth2.php
+++ styleguide/Pages/FullWidth2.php
@@ -17,7 +17,7 @@ class Fullwidth extends Page
         return app(PageFactory::class)->create(1, true, [
             'page' => [
                 'controller' => 'FullWidthController',
-                'title' => 'Custom full-width template',
+                'title' => 'Full width template',
                 'id' => 101109,
                 'content' => [
                     'main' => '<p>This is an example of a custom full-width template and is not selectable in the CMS.</p><p>Page content: '.$this->faker->paragraph(8).'</p>
