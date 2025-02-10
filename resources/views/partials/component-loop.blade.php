c802e77a3b38e04abf6e1ec31f3dd5bcfc660864
Jenny Porada <jennyingles@gmail.com>
2025-02-10 15:31:20 -0500

    Modifying the loop to use one section without an extra container

--- resources/views/partials/component-loop.blade.php
+++ resources/views/partials/component-loop.blade.php
@@ -7,11 +7,12 @@
 --}}
 
 @if(!empty($base['components']))
-    <div class="flex flex-wrap items-start mt:justify-center gap-y-16">
+    <div class="flex flex-wrap items-start mt:justify-center gap-y-gutter-xl">
         @foreach($base['components'] as $componentName => $component)
             @if(!empty($component['data']) && !empty($component['component']['filename']) && \View::exists('components/'.$component['component']['filename']))
-                <section id="{{ Str::slug($componentName) }}" class="{{ !empty($component['component']['sectionClass']) ? $component['component']['sectionClass'] : $component['component']['filename'] }} w-full {{ !empty($component['component']['column-span']) ? 'mt:colspan-'.$component['component']['column-span'] : '' }} {{ ($loop->iteration % 2 === 0) ? 'xbg-gold-100' : 'xbg-green-100' }} relative">
-                    <div class="component__container {{ strpos($component['component']['filename'], 'page-content') !== 0 ? 'px-container' : '' }}">
+                <section id="{{ Str::slug($componentName) }}" class="relative w-full
+                    {{ !empty($component['component']['sectionClass']) ? $component['component']['sectionClass'] : $component['component']['filename'] }} 
+                    {{ !empty($component['component']['column-span']) ? 'px-4 mt:colspan-'.$component['component']['column-span'] : 'px-container' }} ">
                         @if(!empty($component['component']['heading']))
                             @include('partials/heading', ['heading' => $component['component']['heading'], 'headingClass' => 'mt-0 '.($component['component']['headingClass'] ?? ''), 'headingLevel' => !empty($component['component']['headingLevel']) ? $component['component']['headingLevel'] : 'h2'])
                         @endif
@@ -19,7 +20,6 @@
                         @if(!empty($component['data']))
                             @include('components/'.$component['component']['filename'], ['data' => $component['data'], 'component' => $component['component']])
                         @endif
-                    </div>
                 </section>
             @endif
         @endforeach
