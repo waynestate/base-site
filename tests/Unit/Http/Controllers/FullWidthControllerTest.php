2ff9b179f350b5f8fe22bdff8252acb63fe70e77
Chris Pelzer <chrispelzer@gmail.com>
2025-06-09 11:39:08 -0400

    tests: add test coverage for the FullWidthController since it is using a custom styleguide controller to load for the styleguide page

---
+++ tests/Unit/Http/Controllers/FullWidthControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use App\Http\Controllers\FullWidthController;
+use Illuminate\Http\Request;
+use PHPUnit\Framework\Attributes\Test;
+use Tests\TestCase;
+
+final class FullWidthControllerTest extends TestCase
+{
+    #[Test]
+    public function fullwidth_controller_should_show_view(): void
+    {
+        $request = new Request();
+        $request->data = [
+            'base' => [
+                'show_site_menu' => true,
+            ],
+        ];
+
+        $controller = new FullWidthController();
+        $view = $controller->index($request);
+
+        $this->assertFalse($view->getData()['base']['show_site_menu']);
+        $this->assertEquals('childpage', $view->getName());
+    }
+}
