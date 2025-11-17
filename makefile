35ea6e29752c2847eccd61eb2ea745f20b2d6ea5
Chris Pelzer <chrispelzer@gmail.com>
2025-11-17 14:07:41 -0500

    refactor: run `npx @tailwindcss/upgrade` within root of site to upgrade from tailwind3 to tailwind4

--- makefile
+++ makefile
@@ -1,7 +1,7 @@
 # Configuration
 YARNFILE := package.json
 COMPOSERFILE := composer.json
-MIXFILE := webpack.mix.js
+VITEFILE := vite.config.js
 DEPLOY := Envoy.blade.php
 DOTENV := .env
 
@@ -10,8 +10,8 @@ all: install
 install: yarn composerinstall generatekey
 update: yarnupgrade composerupdate
 status: yarncheck
-build: webpackdev
-buildproduction: webpackprod
+build: vitedev
+buildproduction: viteprod
 deploy: install buildproduction runtests envoy
 deployproduction: install buildproduction runtests envoyproduction
 
@@ -38,13 +38,13 @@ composerinstalldev: $(COMPOSERFILE)
 composerinstallproduction: $(COMPOSERFILE)
 	composer install --prefer-dist --no-dev --no-interaction && composer dump-autoload --optimize;
 
-webpackdev: $(MIXFILE)
+vitedev: $(VITEFILE)
 	npm run dev
 
-webpackprod: $(MIXFILE)
+viteprod: $(VITEFILE)
 	npm run build
 
-watch: $(MIXFILE)
+watch: $(VITEFILE)
 	npm run dev-poll
 
 yarnupgrade: $(YARNFILE)
@@ -105,9 +105,6 @@ $(YARNFILE):
 
 $(COMPOSERFILE):
 	composer init
-
-$(MIXFILE):
-	touch $(webpack.mix.js)
 
 $(DOTENV):
 	cp .env.example .env
--- resources/views/components/events-row.blade.php
+++ resources/views/components/events-row.blade.php
@@ -7,12 +7,12 @@
 <ul class="grid gap-8 {{ !empty($component['columns']) && $component['columns'] % 2 == 0 ? ($component['columns'] >= 4 ? ' grid-cols-2' : ' sm-grid-cols-2').' md:grid-cols-3 xl:grid-cols-'.($component['columns']) : ' sm:grid-cols-2 md:grid-cols-3' }}"> 
     @foreach($data as $key => $event)
         <li class="flex items-start break-avoid mb-6">
-            <div class="flex-shrink-0 mt-1 border-2 border-green rounded-sm text-center">
+            <div class="shrink-0 mt-1 border-2 border-green rounded-xs text-center">
                 <div class="w-12 bg-green text-white leading-none border-b-2 border-green text-sm">{{ apdatetime(date('M' , strtotime($event['date']))) }}</div>
                 <div class="text-green text-2xl leading-tight">{{ apdatetime(date('j' , strtotime($event['date']))) }}</div>
             </div>
 
-            <div class="ml-4 flex-grow">
+            <div class="ml-4 grow">
                 <a class="mt-0 block hover:underline" href="{{ $event['url'] }}">
                     {{ $event['title'] }}
                     <span class="visually-hidden"> on {{ apdatetime(date('M d, Y' , strtotime($event['date']))) }}
