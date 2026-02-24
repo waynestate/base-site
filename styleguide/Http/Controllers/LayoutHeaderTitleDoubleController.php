3a55316f0c189c1da5de594d3155c2d49fab6790
Jenny Porada <jennyingles@gmail.com>
2026-02-24 16:25:54 -0500

    Fix: Removing commented code

--- styleguide/Http/Controllers/LayoutHeaderTitleDoubleController.php
+++ styleguide/Http/Controllers/LayoutHeaderTitleDoubleController.php
@@ -28,16 +28,6 @@ class LayoutHeaderTitleDoubleController extends Controller
      */
     public function index(Request $request): View
     {
-        config([
-            'base.surtitle' => $this->faker->sentence($this->faker->numberBetween(2, 4)),
-            'base.surtitle_main_site_enabled' => true,
-            'base.top_menu_enabled' => true,
-            'base.surtitle_url' => '/', 
-        ]);
-
-        //$request->data['base']['hasSurtitle'] = true;
-
-        $request->data['base']['site']['short-title'] = $this->faker->sentence(2);
         $request->data['base']['surtitle'] = $this->faker->sentence($this->faker->numberBetween(2, 4));
         $request->data['base']['surtitle_url'] = '/';
         $request->data['base']['hasSurtitle'] = true;
