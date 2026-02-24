5c438336277b0943f74da51bbd8e57871da0bbdb
Jenny Porada <jennyingles@gmail.com>
2026-02-24 16:23:32 -0500

    Chore: Updating class names that were changed in the template

--- resources/js/modules/sticky.js
+++ resources/js/modules/sticky.js
@@ -9,11 +9,11 @@
         }
 
         if (window.pageYOffset >= offsetHeight) {
-            document.getElementById('panel').style.marginTop = document.querySelector('.menu-top__container').offsetHeight + 'px';
-            document.querySelector('.menu-top__container').classList.add('menu-top--sticky');
+            document.getElementById('panel').style.marginTop = document.querySelector('.nav-top').offsetHeight + 'px';
+            document.querySelector('.nav-top').classList.add('nav-top--sticky');
         } else if(window.pageYOffset < offsetHeight) {
             document.getElementById('panel').style.marginTop = '0px';
-            document.querySelector('.menu-top__container').classList.remove('menu-top--sticky');
+            document.querySelector('.nav-top').classList.remove('nav-top--sticky');
         }
     });
 })();
