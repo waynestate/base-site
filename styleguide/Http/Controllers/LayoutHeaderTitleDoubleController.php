a4832d1e6187dd97a459cf52ba94f6cb68543b79
Jenny Porada <jennyingles@gmail.com>
2026-02-24 13:48:47 -0500

    Fix: Styleguide double header displaying surtitle after menu repository update

--- styleguide/Http/Controllers/LayoutHeaderTitleDoubleController.php
+++ styleguide/Http/Controllers/LayoutHeaderTitleDoubleController.php
@@ -4,6 +4,7 @@ namespace Styleguide\Http\Controllers;
 
 use Illuminate\View\View;
 use App\Http\Controllers\Controller;
+use Contracts\Repositories\MenuRepositoryContract;
 use Illuminate\Http\Request;
 use Faker\Factory;
 
@@ -11,10 +12,15 @@ class LayoutHeaderTitleDoubleController extends Controller
 {
     /**
      * Construct the controller.
+     * @param MenuRepositoryContract $menu
      */
-    public function __construct(Factory $faker)
+    public function __construct(
+        Factory $faker,
+        MenuRepositoryContract $menu
+    )
     {
         $this->faker = $faker->create();
+        $this->menu = $menu;
     }
 
     /**
@@ -26,9 +32,15 @@ class LayoutHeaderTitleDoubleController extends Controller
             'base.surtitle' => $this->faker->sentence($this->faker->numberBetween(2, 4)),
             'base.surtitle_main_site_enabled' => true,
             'base.top_menu_enabled' => true,
+            'base.surtitle_url' => '/', 
         ]);
 
+        //$request->data['base']['hasSurtitle'] = true;
+
         $request->data['base']['site']['short-title'] = $this->faker->sentence(2);
+        $request->data['base']['surtitle'] = $this->faker->sentence($this->faker->numberBetween(2, 4));
+        $request->data['base']['surtitle_url'] = '/';
+        $request->data['base']['hasSurtitle'] = true;
 
         return view('childpage', merge($request->data));
     }
