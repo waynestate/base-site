09028e2ac60a329fda8a7bc83cd439c2bb9fc7e4
Nick DeNardis <nick.denardis@gmail.com>
2026-02-25 12:13:25 -0500

    chore: Blade class attribute and defaults moved to @class directive

--- resources/views/components/article-topics.blade.php
+++ resources/views/components/article-topics.blade.php
@@ -3,7 +3,7 @@
     $heading => string // 'Filter by topic'
     $class => string // ''
 --}}
-<div class="{!! !empty($class) ? $class : 'text-lg m-0 pt-4 font-bold' !!}">{{ $heading }}</div>
+<div @class($class ?? 'text-lg m-0 pt-4 font-bold')>{{ $heading }}</div>
 
 <ul class="main-menu pt-0">
     @foreach($topics as $topic)
