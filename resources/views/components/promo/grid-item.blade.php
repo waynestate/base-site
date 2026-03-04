b78bc3219b8952253d2263d81cfc50e5da3dce63
Jenny Porada <jennyingles@gmail.com>
2026-03-04 14:14:50 -0500

    Feature: Restructuring the promo item file for easier reading and styling

--- resources/views/components/promo/grid-item.blade.php
+++ resources/views/components/promo/grid-item.blade.php
@@ -3,50 +3,47 @@
     $item => array // ['title', 'link', 'description', 'excerpt', 'relative_url', 'option']
 --}}
 
-@if (!empty($item['link']))
-    <a href="{{ $item['link'] }}" class="block {{ !empty($component['gradientOverlay']) && $component['gradientOverlay'] === true ? 'bg-green-800 relative overflow-hidden' : '' }} group">
-@else
-    <div class="block {{ !empty($component['gradientOverlay']) && $component['gradientOverlay'] === true ? 'bg-green-800 relative overflow-hidden' : '' }} {{ $loop->last != true && !empty($component['filename']) && $component['filename'] != 'catalog' ? 'mt-6 mb-8' : '' }}">
-@endif
-    <div class="{{ !empty($component['gradientOverlay']) ? '' : 'mb-2' }}">
-        @if(!empty($item['youtube_id']))
-            <div class="play-video-button">
-                @if(!empty($item['relative_url']))
-                    @image($item['relative_url'], $item['filename_alt_text'], "lazy block w-full")
-                @else
-                    @image('//i.wayne.edu/youtube/'.$item['youtube_id'].'/max', $item['title'], "lazy block w-full")
-                @endif
+<div @class([
+     'promo',
+     'promo--grid',
+     $component['promoItemClass'] ?? '' => @empty(! $component['promoItemClass']),
+     'promo--image-small' => !empty($component['imageSize']) && $component['imageSize'] == "small",
+     'promo--image-right' => !empty($component['imagePosition']) && $component['imagePosition'] == "right",
+     'promo--image-alternate' => !empty($component['imagePosition']) && $component['imagePosition'] == "alternate",
+     'promo--gradient-overlay' => !empty($component['gradientOverlay']) && $component['gradientOverlay'] == true])>
+
+    @empty(! $item['relative_url'])
+        <img data-src="{{ $item['relative_url'] }}" alt="{{ $item['filename_alt_text'] }}" 
+        @class([
+            "promo__image", 
+            "lazy",
+            "promo--play-button" => !empty($item['youtube_id'])
+        ])>
+    @endif
+
+    <div class="promo__content">
+        @empty(! $item['title'])
+            <div class="promo__title">
+                @empty(! $item['link'])<a href="{{ $item['link'] }}" class="promo__link">@endif
+                    {{ $item['title'] }}
+                @empty(! $item['link'])<span class="promo__link-cover"></span></a>@endif
             </div>
-        @elseif(!empty($item['relative_url']))
-            @image($item['relative_url'], $item['filename_alt_text'], "lazy block w-full")
+        @endif
+
+        @empty(! $item['excerpt'])
+            <p class="promo__excerpt">
+                {!! strip_tags($item['excerpt'], ['em', 'strong', 'br', '&ldquo;', '&rdquo;']) !!}
+            </p>
+        @endif 
+
+        @empty(! $item['description'])
+            <div class="promo__description content">
+                {!! !empty($item['link']) ? preg_replace(['"<a href(.*?)>"', '"</a>"'], '', $item['description']) : $item['description'] !!}
+            </div>
+        @endif
+
+        @if(!empty($component['gradientOverlay']) && $component['gradientOverlay'] == true)
+            <div class="promo__gradient"></div>
         @endif
     </div>
-
-    <div class="w-full {{ !empty($component['gradientOverlay']) && $component['gradientOverlay'] === true ? 'bg-gradient-darkest absolute inset-x-0 bottom-0' : '' }}">
-        <div class="content {{ !empty($component['gradientOverlay']) && $component['gradientOverlay'] === true ? 'white-links text-white relative p-4 pt-20 drop-shadow-px' : '' }}">
-            @if(!empty($item['youtube_id']) || !empty($item['relative_url']))
-                <div class="my-1 font-bold {{ !empty($component['columns']) ? ($component['columns'] < 4 ? 'text-lg' : 'text-base') : 'text-xl' }} group-hover:underline group-focus:underline leading-snug xl:leading-tight">
-                    {{ $item['title'] }}
-                </div>
-            @else
-                @include('partials/heading', ['heading' => $item['title'], 'headingLevel' => $component['headingLevel'] ?? 'h3', 'headingClass' => (!empty($component['columns']) ? ($component['columns'] < 3 ? 'text-2xl' : 'text-xl') : 'text-2xl').' group-hover:underline group-focus:underline leading-snug xl:leading-tight '.($component['headingClass'] ?? '')])
-            @endif
-            <div class="{{ !empty($component['columns']) ? ($component['columns'] < 4 ? ($component['columns'] < 3 ? 'text-base md:text-base' : 'text-base md:text-sm xl:text-base') : 'text-sm') : 'text-base' }} {{ !empty($component['gradientOverlay']) && $component['gradientOverlay'] === true ? 'xl:leading-tight' : 'text-black'}}">
-                @if(!empty($item['excerpt']))
-                    <p class="my-1">{!! strip_tags($item['excerpt'], ['em', 'strong', 'br', '&ldquo;', '&rdquo;']) !!}</p>
-                @endif
-                @if(!empty($item['description']))
-                    @if (!empty($item['link']))
-                        <div>{!! preg_replace(['"<a href(.*?)>"', '"</a>"'], '', $item['description']) !!}</div>
-                    @else
-                        <div>{!! $item['description'] !!}</div>
-                    @endif
-                @endif
-            </div>
-        </div>
-    </div>
-@if (!empty($item['link']))
-    </a>
-@else
-    </div>
-@endif
+</div>
--- resources/views/components/promo/list-item.blade.php
+++ resources/views/components/promo/list-item.blade.php
@@ -1,51 +1,49 @@
 {{--
-    This component's image is full width on small views
+    This component's image is 1/4 width on small views
     $item => array // ['title', 'link', 'description', 'excerpt', 'relative_url', 'option']
 --}}
 
-@if (!empty($item['link']))
-    <a href="{{ $item['link'] }}" class="block {{ !empty($component['imageSize']) && $component['imageSize'] === 'small' ? 'flex items-start' : 'md:flex xl:items-center' }} gap-x-3 lg:gap-x-6 group {{ $loop->iteration > 1 && !empty($component['filename']) && $component['filename'] != 'catalog' ? 'mt-6' : '' }}">
-@else
-    <div class="block {{ !empty($component['imageSize']) && $component['imageSize'] === 'small' ? 'flex items-start' : 'md:flex xl:items-center' }} gap-x-3 lg:gap-x-6 {{ $loop->iteration > 1 && !empty($component['filename']) && $component['filename'] != 'catalog' ? 'mt-6' : '' }}">
-@endif
-    @if(!empty($item['youtube_id']) || !empty($item['relative_url']))
-        <div class="shrink-0 grow-0 {{ !empty($component['imageSize']) && $component['imageSize'] === 'small' ? 'w-1/4' : 'md:w-2/5' }}  
-            @if(!empty($component['imagePosition']) && ($component['imagePosition'] === 'right' || ($component['imagePosition'] === 'alternate' && $loop->even))) md:order-2 @endif">
-            @if(!empty($item['youtube_id']))
-                <div class="play-video-button">
-                    @if(!empty($item['relative_url']))
-                        @image($item['relative_url'], $item['filename_alt_text'], "w-full lazy")
-                    @else
-                        @image('//i.wayne.edu/youtube/'.$item['youtube_id'].'/max', $item['title'], "w-full lazy")
-                    @endif
-                </div>
-            @elseif(!empty($item['relative_url']))
-                @image($item['relative_url'], $item['filename_alt_text'], "lazy w-full")
-            @endif
-        </div>
+<div @class([
+     'promo',
+     'promo--list',
+     $component['promoItemClass'] ?? '' => @empty(! $component['promoItemClass']),
+     'promo--image-small' => !empty($component['imageSize']) && $component['imageSize'] == "small",
+     'promo--image-right' => !empty($component['imagePosition']) && $component['imagePosition'] == "right",
+     'promo--image-alternate' => !empty($component['imagePosition']) && $component['imagePosition'] == "alternate",
+     'promo--gradient-overlay' => !empty($component['gradientOverlay']) && $component['gradientOverlay'] == true])>
+
+    @empty(! $item['relative_url'])
+        <img data-src="{{ $item['relative_url'] }}" alt="{{ $item['filename_alt_text'] }}" 
+        @class([
+            "promo__image", 
+            "lazy",
+            "promo--play-button" => !empty($item['youtube_id'])
+        ])>
     @endif
 
-    <div class="content w-full">
-        @if(!empty($item['youtube_id']) || !empty($item['relative_url']))
-            <div class="mb-1 font-bold group-hover:underline group-focus:underline leading-tight text-lg lg:text-xl {{ !empty($component['imageSize']) && $component['imageSize'] === 'small' ? '' : 'mt-2 lg:mt-0' }}">{{ $item['title'] }}</div>
-        @else
-            @include('partials/heading', ['heading' => $item['title'], 'headingLevel' => $component['headingLevel'] ?? 'h3', 'headingClass' => (!empty($component['columns']) ? ($component['columns'] < 3 ? 'text-2xl' : 'text-xl') : 'text-2xl').' group-hover:underline group-focus:underline leading-snug xl:leading-tight '.($component['headingClass'] ?? '')])
+    <div class="promo__content">
+        @empty(! $item['title'])
+            <div class="promo__title">
+                @empty(! $item['link'])<a href="{{ $item['link'] }}" class="promo__link">@endif
+                    {{ $item['title'] }}
+                @empty(! $item['link'])<span class="promo__link-cover"></span></a>@endif
+            </div>
         @endif
-        <div class="text-black">
-            @if(!empty($item['excerpt']))
-                <p class="my-1">{!! strip_tags($item['excerpt'], ['em', 'strong', 'br', '&ldquo;', '&rdquo;']) !!}</p>
-            @endif 
-            @if(!empty($item['description']))
-                @if (!empty($item['link']))
-                    <div>{!! preg_replace(['"<a href(.*?)>"', '"</a>"'], '', $item['description']) !!}</div>
-                @else
-                    <div>{!! $item['description'] !!}</div>
-                @endif
-            @endif
-        </div>
+
+        @empty(! $item['excerpt'])
+            <p class="promo__excerpt">
+                {!! strip_tags($item['excerpt'], ['em', 'strong', 'br', '&ldquo;', '&rdquo;']) !!}
+            </p>
+        @endif 
+
+        @empty(! $item['description'])
+            <div class="promo__description content">
+                {!! !empty($item['link']) ? preg_replace(['"<a href(.*?)>"', '"</a>"'], '', $item['description']) : $item['description'] !!}
+            </div>
+        @endif
+
+        @if(!empty($component['gradientOverlay']) && $component['gradientOverlay'] == true)
+            <div class="promo__gradient"></div>
+        @endif
     </div>
-@if (!empty($item['link']))
-    </a>
-@else
-    </div>
-@endif
+</div>
