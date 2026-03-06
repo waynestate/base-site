5ddf869ee577a262966bb5708bfdb3c3606c342a
Amanda Perkins <amandaperkins@wayne.edu>
2026-03-06 10:51:28 -0500

    Feature/Remove auto-generated alt text for accessibility

--- resources/views/components/profile.blade.php
+++ resources/views/components/profile.blade.php
@@ -1,9 +1,9 @@
 {{--
-    $profile => array // ['First Name, Last Name, Title, link']
+$profile => array // ['First Name, Last Name, Title, link']
 --}}
 <a href="{{ $profile['link'] }}" class="group">
     <div class="aspect-portrait mb-1">
-        @image(!empty($profile['data']['Picture']['url']) ? $profile['data']['Picture']['url'] : '/_resources/images/no-photo.svg', $profile['data']['First Name'].' '.$profile['data']['Last Name'], 'h-full w-full object-cover object-center')
+        @image(!empty($profile['data']['Picture']['url']) ? $profile['data']['Picture']['url'] : '/_resources/images/no-photo.svg', '', 'h-full w-full object-cover object-center')
     </div>
     <div class="underline group-hover:no-underline group-focus:no-underline font-bold">{{ $profile['full_name'] }}</div>
     @if(!empty($profile['data']['Title']))
