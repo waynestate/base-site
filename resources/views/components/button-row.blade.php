fcb3de9f626a11366ef02c2f9b0daa12456cd59c
Jenny Porada <jennyingles@gmail.com>
2026-05-11 12:48:32 -0400

    Style - refining hero button styles

--- resources/views/components/button-row.blade.php
+++ resources/views/components/button-row.blade.php
@@ -2,7 +2,11 @@
     $button => array // ['title', 'link']
 --}}
 
-<ul class="grid md:grid-cols-{{ !empty($component['columns']) && count($data) > 1 ? count($data) % 2 == 0 ? '2' : '3' : '' }} xl:grid-cols-{{ !empty($component['columns']) ? $component['columns'] : '3' }} xl:mx-0 items-start gap-x-4 gap-y-2">
+<ul @class([
+    'buttons',
+    'buttons--cols-'.($component['columns'] ?? 3),
+    'buttons--odd' => (($component['columns'] ?? 3) % 2 != 0),
+    ])>
     @foreach($data as $button)
         <li class="block text-center">
             @if(!empty($button['option']) && $button['option'] === 'Image')
