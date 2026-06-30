40daad3867f7c28b9a743d9beb06495a81061787
Amanda Perkins <amandaperkins@wayne.edu>
2026-06-30 14:35:00 -0400

    feat update formy form to include legend styling

--- styleguide/Http/Controllers/CMSFormsController.php
+++ styleguide/Http/Controllers/CMSFormsController.php
@@ -5,14 +5,27 @@ namespace Styleguide\Http\Controllers;
 use Illuminate\View\View;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Waynestate\FormyParser\Parser;
 
 class CMSFormsController extends Controller
 {
+    /**
+     * Construct the controller.
+     *
+     * @param Parser $parser
+     */
+    public function __construct(Parser $parser)
+    {
+        $this->parser = $parser;
+    }
+
     /**
      * Display the form view.
      */
     public function index(Request $request): View
     {
-        return view('styleguide-cms-forms', merge($request->data));
+        $form['form'] = $this->parser->parse('[form id="base-test"]');
+
+        return view('styleguide-cms-forms', merge($request->data, $form));
     }
 }
