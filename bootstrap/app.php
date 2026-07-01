74481a6b27fd573ca12accaddfbbc89225cdbd47
Shift <shift@laravelshift.com>
2026-07-01 16:24:06 +0000

    Shift core files

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -58,5 +58,8 @@ return Application::configure(basePath: dirname(__DIR__))
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
+        $exceptions->shouldRenderJsonWhen(
+            fn (Request $request) => $request->is('api/*') || $request->expectsJson(),
+        );
         //
     })->create();
