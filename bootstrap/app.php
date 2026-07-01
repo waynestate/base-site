60cda14690270f654fad76c1cb17398d6249118c
Shift <shift@laravelshift.com>
2026-07-01 16:24:24 +0000

    Rename CSRF middleware to PreventRequestForgery

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -19,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->redirectGuestsTo(fn () => route('login'));
         $middleware->redirectUsersTo(AppServiceProvider::HOME);
 
-        $middleware->validateCsrfTokens(except: [
+        $middleware->preventRequestForgery(except: [
             '*',
         ]);
 
