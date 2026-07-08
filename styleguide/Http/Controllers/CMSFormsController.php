43fee71791322c12933cd7d8ad8f978872a74ac4
Amanda Perkins <amandaperkins@wayne.edu>
2026-07-08 12:28:45 -0400

    fix: restore embedded form markup for styling

--- styleguide/Http/Controllers/CMSFormsController.php
+++ styleguide/Http/Controllers/CMSFormsController.php
@@ -5,27 +5,14 @@ namespace Styleguide\Http\Controllers;
 use Illuminate\View\View;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use Waynestate\FormyParser\Parser;
 
 class CMSFormsController extends Controller
 {
-    /**
-     * Construct the controller.
-     *
-     * @param Parser $parser
-     */
-    public function __construct(Parser $parser)
-    {
-        $this->parser = $parser;
-    }
-
     /**
      * Display the form view.
      */
     public function index(Request $request): View
     {
-        $form['form'] = $this->parser->parse('[form id="base-test"]');
-
-        return view('styleguide-cms-forms', merge($request->data, $form));
+        return view('styleguide-cms-forms', merge($request->data));
     }
 }
